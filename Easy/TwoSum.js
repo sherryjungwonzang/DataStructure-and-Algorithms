4f71ac85939ff8b966db2bb8b43067f194fcfbe7
sherry jungwon zang <91277856+jungwonsherryzang@users.noreply.github.com>
2022-05-01 01:31:58 -0400

    Update TwoSum.js

--- Easy/TwoSum.js
+++ Easy/TwoSum.js
@@ -1,1 +1,16 @@
-
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = function(nums, target) {
+    let map = new Map;
+    
+    for (var i = 0; i < nums.length; i++) {
+        let  current = target - nums[i];
+        if (map.has(current)) {
+            return [map.get(current), i]
+        }
+        map.set(nums[i], i);
+    }
+};
