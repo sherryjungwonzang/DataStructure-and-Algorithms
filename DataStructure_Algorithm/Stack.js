26f35117fa544c2c9e893f0c96d25f77c6682db4
sherry jungwon zang <91277856+jungwonsherryzang@users.noreply.github.com>
2022-05-19 22:06:38 -0400

    Update Stack.js

--- DataStructure_Algorithm/Stack.js
+++ DataStructure_Algorithm/Stack.js
@@ -6,73 +6,100 @@
 //push: add element in the last
 //pop: remove the element from the array
 
-const stack = [];
-stack.push("first");
-stack.push("second");
-stack.push("third");
-
-stack //["first", "second", "third"]
-
-stack.pop() //this will remove "third"
-
-
-
-//Using linked list
-class Node {
-  constructor (value) {
-    this.value = value;
-    this.next = null;
-  }
+function Stack(array) {
+    this.array = [];
+    if (array) this.array = array;
 }
 
-class Stack {
-  constructor() {
-    this.first = null;
-    this.last = null;
-    this.size = 0;
-  }
+Stack.prototype.getBuffer = () => {
+    return this.array.slice();
+}
 
-  //push method
-  push(val) {
-    var newNode = new Node(val);
+Stack.prototype.isEmpty = () => {
+    return this.array.length == 0;
+}
 
-    if (!this.first) {
-      this.first = newNode;
-      this.last = newNode;
-    } else {
-      var temp = this.first;
-      this.first = newNode;
-      this.first.next = temp;
+//instance of stack class
+var stack1 = new Stack();
+
+console.log(stack1); //output: {array: []}
+
+
+//Peek
+//to compare the last-added element
+//to evaluate whether the last-added element should be removed from the data structure
+Stack.prototype.peek = () => {
+    return this.array[this.array.length - 1];
+}
+stack1.push(10);
+console.log(stack1.peek()); //10
+stack1.push(5);
+console.log(stack1.peek()); //5
+//Time complexity: O(1)
+
+
+//Insertion
+//with JavaScript arrays
+Stack.prototype.push = (value) => {
+    this.array.push(value);
+}
+stack1.push(1);
+stack1.push(2);
+stack1.push(3);
+console.log(stack1); //{array: [1,2,3]}
+//Time complexity: O(1)
+
+
+//Deletion
+//pop() method
+Stack.prototype.pop = () => {
+    return this.array.pop();
+}
+stack1.pop(1);
+stack1.pop(2);
+stack1.pop(3);
+console.log(stack1); //{array: []}
+//Time complexity: O(1)
+
+
+//Access
+//how to access an element based on order
+function stackAccessNthTopNode(stack, n) {
+    var bufferArray = stack.getBuffer();
+
+    if (n <= 0) throw 'error';
+
+    var bufferStack = new Stack(bufferArray);
+
+    while(--n !== 0) {
+        bufferStack.pop();
     }
-    return ++this.size;
-  }
+    return bufferStack.pop();
+}
 
-  //pop method
-  pop() {
-    if (!this.first) return null;
-
-    var temp = this.first;
-
-    if (this.first === this.last) {
-      this.last = null;
-    }
-    this.first = this.first.next;
-    this.size--;
-
-    return temp.value;
-  }
-} 
-
-const stack = new Stack()
-stack.push(23)
-stack.push(4)
-stack.push(18)
-
-console.log(stack)
+var stack2 = new Stack();
+stack2.push(1);
+stack2.push(2);
+stack2.push(3);
+stackAccessNthTopNode(stack2, 2) //2
+//Time complexity: O(n)
 
 
+//Search
+//for specific element
+//first create a buffer stack so that pop() can be called on that buffer stack
+//original stack is not mutated
+//nothing is removed from it
+function stackSearch(stack, element) {
+    var bufferArray = stack.getBuffer();
+    var bufferStack = new Stack(bufferArray); //copy into buffer
 
-
+    while(!bufferStack.isEmpty()) {
+        if (bufferStack.pop() == element) return true;
+    } 
+    return false;
+}
+//Time complexity: O(n)
 
 
 
