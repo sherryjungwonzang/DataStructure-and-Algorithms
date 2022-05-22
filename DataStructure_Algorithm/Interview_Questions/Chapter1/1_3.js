47b57eeb7e2343f3c52bd55ba85474e878c6c34d
sherry jungwon zang <91277856+jungwonsherryzang@users.noreply.github.com>
2022-05-22 18:18:05 -0400

    Update 1_3.js

--- DataStructure_Algorithm/Interview_Questions/Chapter1/1_3.js
+++ DataStructure_Algorithm/Interview_Questions/Chapter1/1_3.js
@@ -3,32 +3,14 @@
 //string has sufficient space at the end to hold the additional characters
 //given the true length of the string
 
-//O(n) time and O(n) space
-function replaceSpaces(string, length) {
-    const array = string.split('');
-    let spaceCount = 0;
-    
-    for (let i = 0; i < length; i++) {
-        if (array[i] === ' ') spaceCount++;
-    }
-
-    //total amount of space needed
-    let idx = length + spaceCount * 2;
-
-    for (let i = length - 1; i > -1; i--) {
-        const char = array[i];
-
-        if (char === ' ') {
-            array[idx - 1] = '0';
-            array[idx - 2] = '2';
-            array[idx - 3] = '%';
-            idx -= 3;
-        } else {
-            array[idx - 1] = char;
-            idx --;
+const replaceUrlSpace = (str) => {
+    const convertToArray = str.trim().split('');
+    for (let i in convertToArray) {
+        if(convertToArray[i] === " ") {
+            convertToArray[i] = "%20";
         }
     }
-    return array.join('');
+    return convertToArray.join('');
 }
 
-module.exports = replaceSpaces
+console.log(replaceUrlSpace("Sherry J Z")) //Sherry%20J%20Z
