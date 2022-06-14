2eeebc4b34e5422c00aa204aff3bc6284bfb9a79
sherry jungwon zang <91277856+jungwonsherryzang@users.noreply.github.com>
2022-06-13 21:40:15 -0400

    Update TwoSum.js

--- Easy/TwoSum.js
+++ Easy/TwoSum.js
@@ -1,5 +1,5 @@
+//Two Sum
 //Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-
 //You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
 
@@ -8,7 +8,7 @@
  * @param {number} target
  * @return {number[]}
  */
-const twoSum = function(nums, target) {
+const twoSum = (nums, target) => {
     let map = new Map;
     
     for (var i = 0; i < nums.length; i++) {
