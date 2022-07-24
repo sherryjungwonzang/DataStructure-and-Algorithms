af571ed7c7f82eab7ee4e5ecba28d80f0cde5262
sherry jungwon zang <91277856+jungwonsherryzang@users.noreply.github.com>
2022-07-24 18:16:55 -0400

    Create binaryTreeZigzagLevelorderTraveral.js

---
+++ Amazon/binaryTreeZigzagLevelorderTraveral.js
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+
+//given the root of a binary tree, return the zigzag level order traversal of its nodes' values
+var zigzagLevelOrder = function(root) {
+    if (!root) return [];
+    
+    let queue = [root];
+    let output = [];
+    let deep = 0;
+    
+    while(queue.length > 0) {
+        const size = queue.length;
+        const level = [];
+        
+        for (let i = 0; i < size; i++) {
+            const node = queue.shift();
+            if (deep % 2 === 0) {
+                level.push(node.val);
+            } else {
+                level.unshift(node.val);
+            }
+            
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+        output.push(level);
+        deep++;
+    }
+    return output;
+};
