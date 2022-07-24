8d02e9d47f26cad6c157fee3ad165627413ae40a
sherry jungwon zang <91277856+jungwonsherryzang@users.noreply.github.com>
2022-07-24 13:17:49 -0400

    Create trappingRainWater.js

---
+++ Amazon/trappingRainWater.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = (height) => {
+    let res = 0;
+    let i = 0;
+    const st = [];
+    
+    while(i < height.length) {
+        while(st.length !== 0 && height[i] > height[st[st.length - 1]]) {
+            const top = st[st.length - 1];
+            st.pop();
+            
+            if (st.length === 0) break;
+            
+            const dist = i - st[st.length - 1] - 1;
+            const h = Math.min(height[i], height[st[st.length - 1]]) - height[top];
+            res += dist * h;
+        }
+        st.push(i);
+        i++;
+    }
+    return res;
+};
+// time O(n)
+// space O(n)
