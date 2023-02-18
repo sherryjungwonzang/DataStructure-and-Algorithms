71c8a5a7baaf750133d92600efb5ab4e6c7ff626
sherry jungwon zang <91277856+jungwonsherryzang@users.noreply.github.com>
2023-02-18 11:51:36 -0800

    Create SameTree(100).js

---
+++ Blind75/Tree/SameTree(100).js
@@ -0,0 +1,41 @@
+//Same Tree (100)
+//given the roots of two binary trees p and q
+//ro check if they are the same or not
+//two binary trees - considered the same if they arr structually identical, and the nodes have the same value
+
+//Approach:
+//Recursive solution
+//base case: pop this out of the recursive call stack
+//recurrence relation: what is going to get us closer and closer to the base case after each kind of recursive call
+var isSameTree = (p, q) => {
+  //base cases
+  if (p === null && q === null) return true;
+  if (p === null || q === null) return false;
+
+  if (p.val === q.val) {
+    //recurrence relation - both on the left subtree and right subtree
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+  }
+  return false;
+ }
+isSameTree([1,2,3], [1,2,3]); //true
+//p.val = q.val 
+//isSameTree(2, 2) && isSameTree(3,3)
+//no more subtree - true
+
+isSameTree([1,2], [1,null,2]); //false
+//p.val = q.val
+//isSameTree(2, null) && isSameTree(null, 2)
+//p === 2 and q === null -> return false
+// &&
+//p === null and q === 2 -> return false
+//false
+
+isSameTree([1,2,1], [1,1,2]); //false
+//p.val = q.val
+//isSameTree(2, 1) && isSameTree(1, 2)
+//p=2 !== q=1 -> return false
+// &&
+//p=1 !== q=2 -> return false
+//false
+
