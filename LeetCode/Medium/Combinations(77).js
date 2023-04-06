2b6f33fe849b4864b04b097f4e259eac1007cf3d
sherry jungwon zang <91277856+jungwonsherryzang@users.noreply.github.com>
2023-04-05 18:22:39 -0700

    Create Combinations(77).js

---
+++ LeetCode/Medium/Combinations(77).js
@@ -0,0 +1,32 @@
+//77. Combinations
+//given two integers 'n; and 'k' 
+//return all possible combinations of 'k' numbers chosen from the range [1, n]
+//may return the answer in any order
+var combinations = (n, k) => {
+  //to store all possible solutions
+  let result = [];
+  
+  //DFS recursion 
+  function dfs(index, curr) {
+    //base case
+    if (curr.length === k) {
+      result.push([...curr]);
+    }
+
+    for (let i = index; i <= n; i++) {
+      curr.push(i);
+
+      //recurse
+      dfs(i + 1, curr); //recurse on the next position
+
+      //backtracking
+      curr.pop();
+    }
+  }
+  dfs(1, []); //cause this is starting from 1 
+
+  return result;
+}
+combinations(4,2); //[[1,2], [1,3], [1,4], [2,3], [2,4], [3,4]] - there are 4 choose 2 = 6 total combinations
+
+combinations(1,1); //[[1]] - there is 1 choose 1 = 1 total combination
