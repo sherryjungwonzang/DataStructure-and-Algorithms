6ed6e8838a66b1f34b5de27a3a6ca4141d7b62b8
sherry jungwon zang <91277856+jungwonsherryzang@users.noreply.github.com>
2023-04-05 18:34:57 -0700

    Create LongestPalindromeSubstring(5).js

---
+++ NeetCode150/1D_DynamicProgramming/LongestPalindromeSubstring(5).js
@@ -0,0 +1,90 @@
+//Longest Palindrome Substring (5)
+//given a string 's'
+//return the longest palindrome substring in s
+
+//Approach:
+//creating helper function - isPal(s, left, right) -> to check palindrome with left and right pointer
+//setting respectively - even and odd length
+//odd - compare the left and right side on the index
+//even - compare the left and right itself
+
+//*longest = max(longestPal, longest)
+var longestPalindromic = (s) => {
+  let longest = "";
+
+  //to check for palindromes with left and right pointer
+  function isPal(s, left, right) {
+    while(left >= 0 && right < s.length && s[left] === s[right]) {
+      left--;
+      right++;
+    }
+    return s.slice(left + 1, right); //we dont want to include curresnt left values
+  }
+
+  //loop through the strings
+  for (let i = 0; i < s.length; i++) {
+    let oddPal = isPal(s, i, i); //for odd length string - starting at the same index
+    let evenPal = isPal(s, i, i + 1); //even length string - starting at the different index  with i and i+1
+
+    //work out the longest palindrome between the odd and even Pal
+    let longestPal = oddPal.length > evenPal.length ? oddPal : evenPal;
+
+    //check the longesPal against the longest variable
+    if (longestPal.length > longest.length) {
+      //update the longest to longestPal
+      longest = longestPal;
+    }
+  }
+  return longest;
+}
+//TC: O(n^2) - loop through odd length and even length 2 times
+//SC: O(1) - update longest
+longestPalindromic("babad"); //"bab" - "aba" is also a valid answer
+//longest = ""
+//isPal
+//  b a b a d
+//starting from 'b'
+//l
+//  r
+//b -> is palindrome right now and update longest
+//longest = "b"
+
+//starting 'a'
+//  l
+//      r
+//b vs b -> palindrome
+//longest = "bab"
+
+//starting 'b'
+//    l
+//        r
+//a vs a -> palindrome
+//longest = "bab" | "aba" is the same length so no update
+
+//starting 'a'
+//       l
+//           r
+//b vs d -> no palindrome
+
+//starting 'd'
+//          l
+//              r 
+//out of bound
+
+longestPalindromic("cbbd:"); //"bb"
+//longest = ""
+//isPal
+//  c b b d
+//starting from 'c' - is the palindrome
+//  l
+//    r
+//longest = "c"
+
+//    l
+//       r
+//b vs b ->  palindrome
+//longest = "bb"
+
+//       l
+//          r
+//b vs d -> no palindrome
