a5480d39ce4de0c765d702ec9f8276eef7103cda
sherry jungwon zang <91277856+jungwonsherryzang@users.noreply.github.com>
2023-04-11 23:07:23 -0700

    Create DeepestLeavesSum(1302).js

---
+++ LeetCode/Medium/DeepestLeavesSum(1302).js
@@ -0,0 +1,60 @@
+//1302. Deepest Leaves Sum
+//given the 'root' of a binary tree
+//return the sum of values of its deepest leaves
+var deepestLeavesSum = (root) => {
+  //variables
+  let sum = 0;
+  let deepestLevel = 0;
+
+  //DFS
+  function dfs(root, level) {
+    //base case
+    if (!root) return;
+
+    if(level === deepestLevel) {
+      sum += root.val;
+    } else if (level > deepestLevel) {
+      deepestLevel = level;
+      sum = root.val;
+    }
+
+    //recursive calls - left and right side
+    dfs(root.left, level + 1);
+    dfs(root.right, level + 1);
+  }
+  dfs(root, 0);
+
+  return sum;
+}
+deepestLeavesSum([1,2,3,4,5,null,6,7,null,null,null,null,8]); //15
+//        1         -> level 0
+//      2   3       -> level 1
+//    4  5    6     -> level 2
+//  7           8   -> level 3 
+//sum = 0
+//deepestLevel = 0
+
+//starting from root - '1'
+//checking '1' 's level = deepestLevel
+//updating sum = 1
+//deepestLevel = 0
+
+//moving to '2''s level > deepestLevel
+//updating sum = 2
+//resetting deepestLevel = 1
+//...
+//go back to '2' 's right side
+//checking '5' 's level < deepestLevel -> nothing | go back
+//sum = 7
+//deepestLevel = 3
+
+//moving to right side
+//...
+//checking '8' 's level = deepestLevel
+//updating sum = 15
+
+deepestLeavesSum([6,7,8,2,7,1,3,9,null,1,4,null,null,null,5]); //19
+//           6          -> level 0
+//        7     8       -> level 1
+//      2  7   1  3     -> level 2
+//    9   1 4       5   -> level 3
