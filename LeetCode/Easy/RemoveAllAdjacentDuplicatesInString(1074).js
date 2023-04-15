c5c6fb9280c1019ad82cb36ae7f35b8759d5804f
sherry jungwon zang <91277856+jungwonsherryzang@users.noreply.github.com>
2023-04-14 21:30:15 -0700

    Create RemoveAllAdjacentDuplicatesInString(1074).js

---
+++ LeetCode/Easy/RemoveAllAdjacentDuplicatesInString(1074).js
@@ -0,0 +1,50 @@
+//1047. Remove all adjacent duplicates in string
+//given a string's' consisting of lowercase english letters
+//a duplicate removal consiste of choosing two adjacent and equal letters and remving them
+
+//repeatedly make duplicate removals on 's' until we no longer can
+
+//return the final string after all such duplicate removals have been made
+//it can be proven that the answer is unique
+
+//Approach:
+//using  the stack data structure - to keep track of previous value for comparison
+var removeDuplicates = (s) => {
+  //creating the stack
+  let stack = [];
+
+  for (let char of s) {
+    //if they are equal -> add to stack
+    //if not then pop off from the stack
+    stack[stack.length - 1] === char ? stack.pop() : stack.push(char);
+  }
+  //convert it from array format to string format
+  return stack.join("");
+}
+//TC: O(n) - n is the length of the string | loop through array exaclty once
+//SC: O(n) - utilizing the stack data structure | worst case: add all of the values within the string into the stack
+removeDuplicates("abbaca"); //"ca" - can remove "bb" since the letters are adjacent and equal, and then possible move. After moving, "aa" is possible to remove
+//stack = [ ]
+//"a b b a c a"
+// ^
+
+//a has no previous value -> add into stack
+//stack = [ a ]
+
+//   ^
+//a != b -> add into stack
+//stack = [ a b ]
+
+//      ^
+//b = b -> pop off from the stack
+//stack = [ a ]
+
+//         ^
+//a = a -> pop off from the stack
+//stack = [ ]
+
+//          ^
+//add the rest -> stack = [c a]
+//convert it to string -> "ca"
+
+removeDuplicates("azxxzy"); //"ay"
