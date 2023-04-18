4be30bea74157a34b052dd07854e2fcdfea34387
sherry jungwon zang <91277856+jungwonsherryzang@users.noreply.github.com>
2023-04-18 02:16:07 -0700

    Create DiagonalTraverse(498).js

---
+++ LeetCode/Medium/DiagonalTraverse(498).js
@@ -0,0 +1,36 @@
+//498. Diagonal Traverse
+//given an m x n matrix 'mat'
+//return an array of all the elements of the array in a diagonal order
+var diagonalTraverse = (mat) => {
+  //there is only one value 
+  //rather than carrying out a double 
+  if (mat.length === 1) return mat.flat(); //.flat(): creating a new array with all sub-array elements concatenated into it recursively up to the specified depth
+
+  let row = mat.length;
+  let col = mat[0].length;
+
+  //containing five arrays
+  let res = Array.from(Array(row + col - 1), () => new Array().fill([]));
+
+  for (let i = 0; i < row; i++) {
+    for (let j = 0; j < col; j++) {
+      if ((i + j) % 2 === 0) { //at even index
+        //need to get correct array within the arrays
+        //unshift() - to add to the front
+        res[i + j].unshift(mat[i][j]);
+      } else {
+        res[i + j].push(mat[i][j]);
+      }
+    }
+  }
+  //need to return the flatten array
+  return res.flat();
+}
+diagonalTraverse([[1,2,3],[4,5,6],[7,8,9]]); //[1,2,4,7,5,3,6,8,9]
+// 1  2  3
+// 4  5  6
+// 7  8  9
+
+diagonalTraverse([[1,2], [3,4]]); //[1,2,3,4]
+// 1  2
+// 3  4
