e70717a6d81f3a4ceeda079062bdfb185e63b79e
sherryjungwonzang <80478248+sherryjungwonzang@users.noreply.github.com>
2024-05-21 20:01:34 -0700

    Create 131-PalindromePartitioning.js

---
+++ LeetCode/BackTracking/131-PalindromePartitioning.js
@@ -0,0 +1,47 @@
+//131. Palindrome partitioning
+//given a string 's', partition 's' such that every substring of the partition is a palindrome
+//return all possible palindrome partitioning of s
+
+//Approach:
+//using backtracking
+var palindromePartition = (s) => {
+  let res = [];
+
+  //checking palindrome
+  function isPalindrome(str) {
+    let left = 0;
+    let right = str.length - 1;
+
+    while(left < right) {
+      if (str[left] !== str[right]) return false;
+      left++;
+      right--;
+    }
+    return true;
+  };
+
+  function partition (start, arr, res) {
+    if (start === s.length) {
+      res.push([...arr]);
+      return;
+    }
+
+    for (let end = start + 1; end <= s.length; end++) {
+      let subStr = s.substring(start, end);
+
+      if (isPalindrome(subStr)) {
+        arr.push(subStr);
+        partition(end, arr, res);
+        arr.pop();
+      }
+    }
+  };
+
+  partition(0, [], res);
+
+  return res;
+}
+//TC: O(2^n)
+palindromePartition("aab"); //[["a", "a", "b"], ["aa", "b"]]
+
+palindromePartition("a"); //[["a"]]
