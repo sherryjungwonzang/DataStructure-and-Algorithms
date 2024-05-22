4077c715354fba5b3eaaa42b57a94fec7f61c37d
sherryjungwonzang <80478248+sherryjungwonzang@users.noreply.github.com>
2024-05-21 23:11:37 -0700

    Create 283-MoveZeroes.js

---
+++ LeetCode/TwoPointers/283-MoveZeroes.js
@@ -0,0 +1,47 @@
+//283. Move Zeroes
+//given an integer array 'nums'
+//move all 0's to the end of it while maintaining the relative order of the non-zero elements
+
+//Approach:
+//two pointers - I will use this in this question | since more efficient
+var moveZeroes = (nums) => {
+    //two pointers at the same index
+    let left = 0;
+    let right = 0;
+
+    while(right < nums.length) {
+        if (nums[right] !== 0) {
+            //flip with L and R pointer
+            [nums[left], nums[right]] = [nums[right], nums[left]];
+            left++;
+        }
+        right++;
+    }
+}
+//TC: O(n)
+//SC: O(1)
+moveZeroes([0,1,0,3,12]); //[1,3,12,0,0]
+//[0, 1, 0, 3, 12]
+// l
+// r
+
+// l
+//    r
+//r !== 0 -> flip with left
+
+//[1, 0, 0, 3, 12]
+//    l
+//       r
+//move right since r is 0
+//    l
+//          r
+//r !== 0 -> flip with left
+
+//[1, 3, 0, 0, 12]
+//       l
+//              r
+//r !== 0 -> flip with left
+
+//[1, 3, 12, 0, 0]
+
+moveZeroes([0]); //[0]
