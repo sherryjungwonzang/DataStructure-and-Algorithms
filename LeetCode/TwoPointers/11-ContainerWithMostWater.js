e42e7196f709bfdba2b7868b1fc2c08cb8997a5e
sherryjungwonzang <80478248+sherryjungwonzang@users.noreply.github.com>
2024-05-21 23:12:06 -0700

    Create 11-ContainerWithMostWater.js

---
+++ LeetCode/TwoPointers/11-ContainerWithMostWater.js
@@ -0,0 +1,60 @@
+//11. Container with most water
+//given an integer array 'height' of length 'n'
+//there are n vertical lines drawn such that the two endpoints of the i_th line are (i, 0) and (i, height)
+
+//find two lines that together with the x-axis form a container
+//such that the container contains the most water
+//return the max amount of water a container can store
+
+//Approach:
+//using the left and right pointer
+//area formula = height * width
+//check the area with the smallest value of heights
+//move pointers to inwards based on the smallest value
+var containerWithMaxWater = (height) => {
+    //two pointers
+    let left = 0;
+    let right = height.length - 1;
+    let max = 0;
+
+    while(left < right) {
+        //setting width
+        let width = (right - left);
+        //setting max area
+        let maxArea = Math.min(height[left], height[right]) * width;
+
+        //update max value
+        max = Math.max(max, maxArea);
+
+        //move pointer whose value is smaller
+        if (height[left] <= height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+    return max;
+}
+containerWithMaxWater([1,8,6,2,5,4,8,3,7]); //49
+//max = 0
+//[1, 8, 6, 2, 5, 4, 8, 3, 7]
+// l                       r
+
+//width: (9-1) * height: 1 = 8
+//max = 0 -> 8
+
+//    l                    r
+//width: (9-2) * height: 7 = 49
+//max = 0 -> 8 -> 49
+
+//    l                  r
+//    l                    r
+//width: (8-2) * height: 3 = 18
+//max = 0 -> 8 -> 49
+//...
+
+
+containerWithMaxWater([1,1]); //1
+//max = 0
+//width: (2-1) * height: 1 = 1
+//max = 0 -> 1
