7f80f7877b1d1c6f3b5e764399fa21efa82ab757
sherryjungwonzang <80478248+sherryjungwonzang@users.noreply.github.com>
2024-05-21 22:51:02 -0700

    Create 257-BinaryTreePaths.js

---
+++ LeetCode/Tree/257-BinaryTreePaths.js
@@ -0,0 +1,40 @@
+//257. Binary Tree Paths
+//given the 'root' of a binary tree
+//return all root-to-leaf paths in any order
+
+//Approach:
+//using DFS with Recursive in each left and right child
+var binaryTreePaths = (root) => {
+    //edge case
+    if (!root) return [];
+
+    let res = [];
+
+    //DFS
+    function dfs(root, path) {
+        path.push(root.val);
+
+        //checking leaf node
+        if (!root.left && !root.right) {
+            //update res in "1->2->5" format
+            //.join(): convert it from an array to string and join with "->"
+            res.push(path.join("->"));
+        }
+
+        //recurse down to left and right
+        if (root.left) dfs(root.left, path);
+        if (root.right) dfs(root.right, path);
+
+        //backtracking
+        path.pop();
+    }
+    dfs(root, []); //[] for current path
+
+    return res;
+}
+binaryTreePaths([1,2,3,null,5]); //["1->2->5","1->3"]
+//   1
+// 2  3
+//  5
+
+binaryTreePaths([1]); //["1"]
