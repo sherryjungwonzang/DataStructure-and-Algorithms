6ab44f5560729622f71ff86e2b1bfcd851a19402
sherryjungwonzang <80478248+sherryjungwonzang@users.noreply.github.com>
2024-08-12 15:38:45 -0700

    Update 746-MinCostClimbingStairs.js

--- LeetCode/DP/746-MinCostClimbingStairs.js
+++ LeetCode/DP/746-MinCostClimbingStairs.js
@@ -5,28 +5,36 @@
 //return the minimum cost to reach the top of the floor
 
 //Approach:
-//using DP array - to store min cost to reach each step
-var minCostClimpingStairs = (cost) => {
-  //creating DP with array - will create as 'cost' array length
-  let dp = new Array(cost);
+//using DP array
+var minCostClimbingStairs = (cost) => {
+    let dp = new Array(cost);
 
-  dp[0] = 0;
-  dp[1] = 0;
+    //no cost at starting point
+    dp[0] = 0;
+    dp[1] = 0;
 
-  //looping steps from 2nd step to n_th step
-  for (let i = 2; i <= cost.length; i++) {
-    dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
-  }
-  return dp[dp.length - 1];
+    //traversing
+    for (let i = 2; i <= cost.length; i++) {
+        //two options
+        dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+    }
+
+    return dp[dp.length - 1];
 }
 //TC: O(n)
 //SC: O(n)
-minCostClimpingStairs([10,15,20]); //15
-//will start at index 1
-//pay 15 and climb two steps to reach the top
-//the total cost is 15
+minCostClimbingStairs([10,15,20]); //15
+//dp = [10, 15, 20] -> [0, 0, 20]
 
-minCostClimpingStairs([1,100,1,1,1,100,1,1,100,1]); //6
+//[10, 15, 20]
+//          ^
+//dp[2] = min(dp[1] + cost[1], dp[0] + cost[0]) = (0 + 15, 0 + 10) = 10
+//dp = [0, 0, 10]
+
+//dp[3] = min(dp[2] + cost[2], dp[1] + cost[1]) = (10 + 20, 0 + 15) = 15
+//dp = [0, 0, 10, 15]
+
+minCostClimbingStairs([1,100,1,1,1,100,1,1,100,1]); //6
 //will start at index 0
 //pay1 and climb two steps to reach index 2
 //pay1 and climb two steps to reach index 4
@@ -35,3 +43,5 @@ minCostClimpingStairs([1,100,1,1,1,100,1,1,100,1]); //6
 //pay1 and climb two steps to reach index 9
 //pay1 and climb two steps to reach index top
 //the total cost is 16
+
+
