49c3611a532aa6f3e63ff77f54b8b5716d22fa92
sherryjungwonzang <80478248+sherryjungwonzang@users.noreply.github.com>
2024-08-14 00:06:53 -0700

    Update 416-PartitionEqualSubsetSum.js

--- LeetCode/DP/416-PartitionEqualSubsetSum.js
+++ LeetCode/DP/416-PartitionEqualSubsetSum.js
@@ -6,22 +6,60 @@
 //Approach:
 //using DP
 var partitionSubsetSum = (nums) => {
-  //getting the sum of all elements
-  let sum = nums.reduce((prev, curr) => prev + curr, 0);
-  if (sum % 2 !== 0) return false; //odd sum - cannot partitioned into two subsets
-  let target = sum / 2; //never have decimal
+    let sum = nums.reduce((prev, curr) => prev + curr, 0)
+    let target = sum / 2; //never have decimal
 
-  //DP
-  let dp = new Array(target + 1).fill(0);
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = target; j >= nums[i]; j--) { //from target sum to curr
-      dp[j] = Math.max(dp[j], dp[j - nums[i]] + nums[i]); 
+    //checking odd or even
+    if (sum % 2 !== 0) return false; //odd - cannot be partitioned into two subsets
+
+    //DP
+    let dp = new Array(target + 1).fill(0);
+
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = target; j >= nums[i]; j--) { //from target sum to curr
+            //max sum can be achieved at curr sum + curr num
+            dp[j] = Math.max(dp[j], dp[j - nums[i]] + nums[i]);
+        }
     }
-  }
-  return dp[target] === target; //meaning possible to partition the array into two equal subsets
+
+    return dp[target] === target; //possible to partition into two subsets
 }
 partitionSubsetSum([1,5,11,5]); //true
 //the array can be partitioned as [1,5,5] and [11]
+//sum = 22 -> not odd
+//target = 11
+
+//DP
+//[ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
+
+//i = 0, target = 11
+//dp[11] = max(0, dp[11 - 1] + 1) = 1
+//dp[10] = max(0, dp[10 - 1] + 1) = 1
+//...
+//dp[0] = max(0, dp[0 - 1] + 1) = 1
+//DP = [ 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ]
+
+//i = 1, target = 11
+//dp[11] = max(1, dp[11 - 5] + 5) = 6
+//dp[10] = max(1, dp[10 - 5] + 5) = 6
+//dp[9] = max(1, dp[9 - 5] + 5) = 6
+//dp[8] = max(1, dp[8 - 5] + 5) = 6
+//dp[7] = max(1, dp[7 - 5] + 5) = 6
+//dp[6] = max(1, dp[6 - 5] + 5) = 6
+//dp[5] = max(1, dp[5 - 5] + 5) = 5
+//DP = [ 0, 1, 1, 1, 1, 5, 6, 6, 6, 6, 6, 6 ]
+
+//i = 2, target = 11
+//dp[11] = max(6, dp[11 - 11] + 11) = 11
+//dp[10] = max(6, dp[10 - 11] + 11) = -
+//DP = [ 0, 1, 1, 1, 1, 5, 6, 6, 6, 6, 6, 11 ]
+
+//i = 5, target = 11
+//dp[11] = max(11, dp[11 - 5] + 5) = 11
+//dp[10] = max(11, dp[10 - 5] + 5) = 10
+//dp[9] = max(11, dp[9 - 5] + 5) = 11
+//DP = [ 0, 1, 1, 1, 1, 5, 6, 6, 6, 6, 10, 11 ]
+//dp[11] = 11 -> true
 
 partitionSubsetSum([1,2,3,5]); //false
 //the array cannot be partitioned into equal sum subsets
