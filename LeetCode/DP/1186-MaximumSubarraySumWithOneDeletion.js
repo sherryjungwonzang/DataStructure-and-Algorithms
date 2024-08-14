56afadbe237b08fb2274af1aef42379820ed95e2
sherryjungwonzang <80478248+sherryjungwonzang@users.noreply.github.com>
2024-08-14 02:20:21 -0700

    Update 1186-MaximumSubarraySumWithOneDeletion.js

--- LeetCode/DP/1186-MaximumSubarraySumWithOneDeletion.js
+++ LeetCode/DP/1186-MaximumSubarraySumWithOneDeletion.js
@@ -7,22 +7,44 @@
 //Approach:
 //using DP
 var maxSubarrayWithOneDeletion = (arr) => {
-  let oneDeletion = 0;
-  let noDeletion = arr[0];
-  let max = arr[0];
+    let oneDeletion = 0;
+    let noDeletion = arr[0];
+    let max = arr[0];
 
-  for (let i = 1; i < arr.length; i++) {
-    oneDeletion = Math.max(oneDelete + arr[i], noDelete);
-    noDeletion = Math.max(noDelete + arr[i], arr[i]);
-    max = Math.max(max, Matj.max(noDeletion, oneDeletion));
-  }
+    for (let i = 1; i < arr.length; i++) {
+        //DP
+        oneDeletion = Math.max(oneDeletion + arr[i], noDeletion);
+        noDeletion = Math.max(noDeletion + arr[i], arr[i]);
+        max = Math.max(max, Math.max(noDeletion, oneDeletion));
+    }
 
-  return max;
+    return max;
 }
 //TC: O(n)
 //SC: O(1)
-maxSubarrayWithOneDeletion([1, -2, 0, 3]); //4
-//Because we can choose [1, -2, 0, 3] and drop -2, thus the subarray [1, 0, 3] becomes the maximum value
+maxSubarrayWithOneDeletion([1, -2, 0, 3]); //4 - Because we can choose [1, -2, 0, 3] and drop -2, thus the subarray [1, 0, 3] becomes the maximum value
+//[1, -2, 0, 3]
+//oneDeletion = 0
+//noDeletion = 1
+//max = 1
+
+//[1, -2, 0, 3]
+//     ^
+//oneDeletion = 0 -> max(0 + -2, 1) = 1
+//noDeletion = 1 -> max(1 + -2, -2) = -1
+//max = 1 -> max(1, max(1, -1)=1) =1
+
+//[1, -2, 0, 3]
+//        ^
+//oneDeletion = 0 -> 1 -> max(1 + 0, -1) = 1
+//noDeletion = 1 -> -1 -> max(-1 + 0, 0) = 0
+//max = 1 -> 1 -> max(1, max(1, 0)=1) =1
+
+//[1, -2, 0, 3]
+//           ^
+//oneDeletion = 0 -> 1 -> 1 -> max(1 + 3, 0) = 4
+//noDeletion = 1 -> -1 -> 0 -> max(0 + 3, 3) = 3
+//max = 1 -> 1 -> 1 -> max(1, max(4, 3)=4) =4
 
 maxSubarrayWithOneDeletion([1,-2,-2,3]); //3 - We just choose [3] and it's the maximum sum
 
