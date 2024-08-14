013b6cbb531e47d2392ce345a9beee915fc2b2e7
sherryjungwonzang <80478248+sherryjungwonzang@users.noreply.github.com>
2024-08-14 00:40:24 -0700

    Update 718-MaximumLengthOfRepeatedSubarray.js

--- LeetCode/DP/718-MaximumLengthOfRepeatedSubarray.js
+++ LeetCode/DP/718-MaximumLengthOfRepeatedSubarray.js
@@ -3,23 +3,35 @@
 //return the max length of a subarray that appears in both arrays
 
 //Approach:
-//using DP
+//using DP with 2D Array
 var maxLenRepeatedSubarray = (nums1, nums2) => {
-  //2D array
-  let dp = new Array(nums1.length + 1).fill(0).map(() => new Array(nums2.length + 1).fill(0));
-  let maxLen = 0;
+    //2D array
+    let dp = new Array(nums1.length + 1).fill(0).map(() => new Array(nums2.length + 1).fill(0));
+    let maxLen = 0;
 
-  //DP
-  for (let i = 1; i <= nums1.length; i++) {
-    for (let j = 1; j <= nums2.length; j++) {
-      if (nums1[i - 1] === nums2[j - 1]) {
-        dp[i][j] = 1 + dp[i - 1][j - 1];
-        maxLen = Math.max(maxLen, dp[i][j]);
-      }
+    //DP
+    for (let i = 1; i <= nums1.length; i++) {
+        for (let j = 1; j <= nums2.length; j++) {
+            //need to find the common num
+            if (nums1[i - 1] === nums2[j - 1]) {
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+                maxLen = Math.max(maxLen, dp[i][j]);
+            }
+        }
     }
-  }
-  return maxLen;
+
+    return maxLen;
 }
 maxLenRepeatedSubarray([1,2,3,2,1], [3,2,1,4,7]); //3 - the repeated subarray with max length is [3,2,1]
+//DP - matching num's position
+//[
+//          3  2  1  4  7 
+//     [ 0, 0, 0, 0, 0, 0 ],
+//  1  [ 0, 0, 0, 1, 0, 0 ],
+//  2  [ 0, 0, 1, 0, 0, 0 ],
+//  3  [ 0, 1, 0, 0, 0, 0 ],
+//  2  [ 0, 0, 2, 0, 0, 0 ],
+//  1  [ 0, 0, 0, 3, 0, 0 ]
+//]
 
 maxLenRepeatedSubarray([0,0,0,0,0], [0,0,0,0,0]); //5 - the repeated subarray with max length is [0,0,0,0,0]
