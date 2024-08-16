5e15faf9388da9b2644904aea26d228d30d67cc8
sherryjungwonzang <80478248+sherryjungwonzang@users.noreply.github.com>
2024-08-15 23:36:23 -0700

    Update 494-TargetSum.js

--- LeetCode/DP/494-TargetSum.js
+++ LeetCode/DP/494-TargetSum.js
@@ -9,28 +9,26 @@
 //return the num of different expressions that you can cuild which evaluates to target
 
 //Approach:
-//using DP - iterate array and pre-calculate the next sums based on previous sums and amounts
+//using DP
 var targetSum = (nums, target) => {
-  //initial setting DP
-  //initial sum to 0 and amount to 1
-  let count = new Map([[0, 1]]);
+    let count = new Map([[0, 1]]); //[sum, amount]
 
-  //DP
-  for (let num of nums) {
-    let next = new Map();
+    //DP
+    for (let num of nums) {
+        let next = new Map();
 
-    for (let [sum, amount] of count) {
-      let plus = sum + num;
-      let minus = sum - num;
+        for (let [sum, amount] of count) {
+            let plus = sum + num;
+            let minus = sum - num;
 
-      next.set(plus, next.has(plus) ? next.get(plus) + amount : amount);
-      next.set(minus, next.has(minus) ? next.get(minus) + amount : amount);
-    }
+            next.set(plus, next.has(plus) ? next.get(plus) + amount : amount);
+            next.set(minus, next.has(minus) ? next.get(minus) + amount : amount);
+        };
 
-    count = next; //updating
-  }
+        count = next; //updating
+    };
 
-  return count.has(target) ? count.get(target) : 0;
+    return count.has(target) ? count.get(target) : 0;
 }
 targetSum([1,1,1,1,1], 3); //5
 //-1 + 1 + 1 + 1 + 1 = 3
@@ -38,5 +36,97 @@ targetSum([1,1,1,1,1], 3); //5
 //+1 + 1 - 1 + 1 + 1 = 3
 //+1 + 1 + 1 - 1 + 1 = 3
 //+1 + 1 + 1 + 1 - 1 = 3
+
+//count = { 0: 1 }
+//next = { }
+
+//[1, 1, 1, 1, 1]
+// ^
+//num = 1, sum = 0, amount = 1
+//plus = 0 + 1 = 1
+//minus = 0 - 1 = -1
+//next = { 1: 1, -1: 1 }
+//count = { 0: 1 } => { 1: 1, -1: 1 }
+
+//[1, 1, 1, 1, 1]
+//    ^
+//num = 1, sum = 1, amount = 1
+//plus = 1 + 1 = 2
+//minus = 1 - 1 = 0
+//next = { 2: 1, 0: 1 }
+
+//num = 1, sum = -1, amount = 1
+//plus = -1 + 1 = 0
+//minus = -1 - 1 = -2
+//next = { 2: 1, 0: 1 + 1 = 2, -2: 1 }
+//count = { 0: 1 } => { 1: 1, -1: 1 } -> { 2: 1, 0: 2, -2: 1 }
+
+//[1, 1, 1, 1, 1]
+//       ^
+//num = 1, sum = 2, amount = 1
+//plus = 2 + 1 = 3
+//minus = 2 - 1 = 1
+//next = { 3: 1, 1: 1 }
+
+//num = 1, sum = 0, amount = 2
+//plus = 0 + 1 = 1
+//minus = 0 - 1 = -1
+//next = { 3: 1, 1: 1 + 2 = 3, -1: 2 }
+
+//num = 1, sum = -2, amount = 1
+//plus = -2 + 1 = -1 
+//minus = -2 - 1 = -3
+//next = { 3: 1, 1: 3, -1: 1 + 2 = 3, -3: 1 }
+//count = { 0: 1 } => { 1: 1, -1: 1 } -> { 2: 1, 0: 2, -2: 1 } -> { 3: 1, 1: 3, -1: 3, -3: 1 }
+
+//[1, 1, 1, 1, 1]
+//          ^
+//num = 1, sum = 3, amount = 1
+//plus = 3 + 1 = 4 
+//minus = 3 - 1 = 2
+//next = { 4: 1, 2: 1 }
+
+//num = 1, sum = 1, amount = 3
+//plus = 1 + 1 = 2
+//minus = 1 - 1 = 0
+//next = { 4: 1, 2: 1 + 3 = 4, 0: 3 }
+
+//num = 1, sum = -1, amount = 3
+//plus = -1 + 1 = 0
+//minus = -1 - 1 = -2
+//next = { 4: 1, 2: 4, 0: 3 + 3 = 6, -2: 3 }
+
+//num = 1, sum = -3, amount = 1
+//plus = -3 + 1 = -2
+//minus = -3 - 1 = -4
+//next = { 4: 1, 2: 4, 0: 6, -2: 3 + 1 = 4, -4: 1 }
+//count = { 0: 1 } => { 1: 1, -1: 1 } -> { 2: 1, 0: 2, -2: 1 } -> { 3: 1, 1: 3, -1: 3, -3: 1 } ->  { 4: 1, 2: 4, 0: 6, -2: 4, -4: 1 }
+
+//[1, 1, 1, 1, 1]
+//             ^
+//num = 1, sum = 4, amount = 1
+//plus = 4 + 1 = 5 
+//minus = 4 - 1 = 3
+//next = { 5: 1, 3: 1 }
+
+//num = 1, sum = 2, amount = 4
+//plus = 2 + 1 = 3
+//minus = 2 - 1 = 1
+//next = { 5: 1, 3: 1 + 4 = 5, 1: 4 }
+
+//num = 1, sum = 0, amount = 6
+//plus = 0 + 1 = 1
+//minus = 0 - 1 = -1
+//next = { 5: 1, 3: 5, 1: 4 + 6 = 10, -1: 6 }
+
+//num = 1, sum = -2, amount = 4
+//plus = -2 + 1 = -1
+//minus = -2 - 1 = -3
+//next = { 5: 1, 3: 5, 1: 10, -1: 6 + 4 = 10, -3: 4 }
+
+//num = 1, sum = -4, amount = 1
+//plus = -4 + 1 = -3
+//minus = -4 - 1 = -5
+//next = { 5: 1, 3: 5, 1: 10, -1: 10, -3: 4 + 1 = 5, -5: 1 }
 
 targetSum([1], 1); //1
