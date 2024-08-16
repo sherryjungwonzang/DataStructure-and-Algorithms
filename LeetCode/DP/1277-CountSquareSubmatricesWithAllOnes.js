dd7afb8821a1bd697675bf2a000b269fdbd6af96
sherryjungwonzang <80478248+sherryjungwonzang@users.noreply.github.com>
2024-08-15 23:56:25 -0700

    Update 1277-CountSquareSubmatricesWithAllOnes.js

--- LeetCode/DP/1277-CountSquareSubmatricesWithAllOnes.js
+++ LeetCode/DP/1277-CountSquareSubmatricesWithAllOnes.js
@@ -5,34 +5,53 @@
 //Approach:
 //using DP
 var countSquareSubmatrices = (matrix) => {
-  let count = 0;
-  let m = matrix.length;
-  let n = matrix[0].length;
+    let count = 0;
+    let m = matrix.length;
+    let n = matrix[0].length;
 
-  //DP
-  for (let i = 0; i < m; i++) {
-    for (let j = 0; j < n; j++) {
-      if (matrix[i][j] === 0) continue; 
+    //DP
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (matrix[i][j] === 0) continue;
 
-      if (i > 0 && j > 0) {
-        //only check diagonal, left side and top side
-        matrix[i][j] += Math.min(matrix[i][j - 1], matrix[i - 1][j], matrix[i - 1][j - 1]);
-      }
+            //check left, top, diagonal side
+            if (i > 0 && j > 0) matrix[i][j] += Math.min(matrix[i][j - 1], matrix[i - 1][j], matrix[i - 1][j - 1]);
 
-      count += matrix[i][j]; //need to count every value
+            count += matrix[i][j]; //need to count every value
+        }
     }
-  }
-  return count;
+
+    return count;
 }
 countSquareSubmatrices([
   [0,1,1,1],
   [1,1,1,1],
   [0,1,1,1]
-]); //15
-//There are 10 squares of side 1.
-//There are 4 squares of side 2.
-//There is  1 square of side 3.
+]); //15 - There are 10 squares of side 1, 4 squares of side 2, 1 square of side 3
 //Total number of squares = 10 + 4 + 1 = 15.
+
+//m = 3, n = 4
+//count = 0
+//i = 0, j = 0 -> "0"
+//i = 0, j = 1 -> continue
+//i = 0, j = 2 -> continue
+//i = 0, j = 3 -> continue
+
+//i = 1, j = 0 -> continue
+//i = 1, j = 1 -> matrix[1][1] += min(1, 1, 0) = 1 + 0 = 1
+//i = 1, j = 2 -> matrix[1][2] += min(1, 1, 1) = 1 + 1 = 2
+//i = 1, j = 3 -> matrix[1][3] += min(1, 1, 1) = 1 + 1 = 2
+//  [0,1,1,1]
+//  [1,1,2,2]
+//  [0,1,1,1]
+
+//i = 2, j = 0 -> continue
+//i = 2, j = 1 -> matrix[2][1] += min(0, 1, 1) = 1 + 0 = 1
+//i = 2, j = 2 -> matrix[2][2] += min(1, 2, 1) = 1 + 1 = 2
+//i = 2, j = 3 -> matrix[2][3] += min(1, 2, 2) = 2 + 1 = 3
+//  [0,1,1,1]
+//  [1,1,2,2]
+//  [0,1,2,3]
 
 countSquareSubmatrices([
   [1,0,1],
