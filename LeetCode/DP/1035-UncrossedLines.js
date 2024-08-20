55b4fc17159c7b01722447fe51079b385b89e1c5
sherryjungwonzang <80478248+sherryjungwonzang@users.noreply.github.com>
2024-08-19 19:34:17 -0700

    Update 1035-UncrossedLines.js

--- LeetCode/DP/1035-UncrossedLines.js
+++ LeetCode/DP/1035-UncrossedLines.js
@@ -18,7 +18,7 @@ var uncrossedLines = (nums1, nums2) => {
     for (let i = 1; i <= m; i++) {
         for (let j = 1; j <= n; j++) {
             //matching
-            if (nums1[i - 1] === nums[j - 1]) dp[i][j] += dp[i - 1][j - 1] + 1; //diagonal
+            if (nums1[i - 1] === nums2[j - 1]) dp[i][j] += dp[i - 1][j - 1] + 1; //diagonal
             else dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1], dp[i][j]); //checking left, top and itself
         }
     };
