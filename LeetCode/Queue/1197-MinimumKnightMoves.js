3f1a0614a72e9ce2fd1c4c20aeedbbc82a467d7f
sherryjungwonzang <80478248+sherryjungwonzang@users.noreply.github.com>
2024-08-22 13:42:07 -0700

    Update 1197-MinimumKnightMoves.js

--- LeetCode/Queue/1197-MinimumKnightMoves.js
+++ LeetCode/Queue/1197-MinimumKnightMoves.js
@@ -12,12 +12,10 @@
 //Approach:
 //using BFS and queue
 var minKnightMoves = (x, y) => {
-    //dirs
+    //8 directions
     let dir = [ [-2, -1], [-1, -2], [1, -2], [2, -1], [2, 1], [1, 2], [-1, 2], [-2, 1] ];
-
     let seen = new Set(); //to track all visited positions
-    let queue = [[0, 0]];
-    let steps = 0;
+    let queue = [[0, 0, 0]];
 
     //BFS
     while (queue.length) {
@@ -25,29 +23,55 @@ var minKnightMoves = (x, y) => {
 
         while(queue.length) {
             //extracting current position
-            let curr = queue.shift();
-            let currX = curr[0];
-            let currY = curr[1];
+            let [currX, currY, steps] = queue.shift(); //curr
 
             if (currX === x & currY === y) return steps;
 
             //check all possible positions
             for (let d of dir) {
                 //extracting next position
-                let nextX = currX + d[0];
-                let nextY = currY + d[1];
+                let [nextX, nextY] = [currX + d[0], currY + d[1]]
 
                 if (!seen.has(nextX + "," + nextY)) {
                     seen.add(nextX + "," + nextY);
-                    next.push([nextX, nextY]);
+                    next.push([nextX, nextY, steps + 1]);
                 }
             }
         }
-        steps++; //after looping all the possible directions
 
         queue = next; //updating queue
     }
 }
-minKnightMoves(2, 1); //1 - [0, 0] -> [2, 1]
-
 minKnightMoves(5, 5); //4 - [0,0] -> [2,1] -> [4,2] -> [3,4] -> [5,5]
+//seen = { }
+//steps = 0
+//queue = [ [0, 0, 0] ]
+
+//curr = [0, 0, 0]
+//nextX          nextY
+// 0 - 2 = -2       0 - 1 = -1 
+// 0 - 1 = -1       0 - 2 = -2
+// 0 + 1 = 1       0 - 2 = -2
+// 0 + 2 = 2       0 - 1 = -1
+// 0 + 2 = 2       0 + 1 = 1
+// 0 + 1 = 1       0 + 2 = 2 
+// 0 - 1 = -1       0 + 2 = 2 
+// 0 - 2 = -1       0 + 1 = 1 
+//seen = { [-2, -1], [-1, -2], [1, -2], [2, -1], [2, 1], [1, 2], [-1, 2], [-1, 1] }
+//next = [ [-2, -1, 1], [-1, -2, 1], [1, -2, 1], [2, -1, 1], [2, 1, 1], [1, 2, 1], [-1, 2, 1], [-1, 1, 1] ]
+
+//queue = [ [-2, -1, 1], [-1, -2, 1], [1, -2, 1], [2, -1, 1], [2, 1, 1], [1, 2, 1], [-1, 2, 1], [-1, 1, 1] ]
+//curr = [-2, -1, 1]
+//nextX          nextY
+// -2 - 2 = -4       -1 - 1 = -2
+// -2 - 1 = -3       -1 - 2 = -3
+// -2 + 1 = -1       -1 - 2 = -3
+// -2 + 2 = 0       -1 - 1 = -2
+// -2 + 2 = 0       -1 + 1 = 0
+// -2 + 1 = -1       -1 + 2 = 1 
+// -2 - 1 = -3       -1 + 2 = 1 
+// -2 - 2 = -4       -1 + 1 = 0
+//... 
+
+
+minKnightMoves(2, 1); //1 - [0, 0] -> [2, 1]
