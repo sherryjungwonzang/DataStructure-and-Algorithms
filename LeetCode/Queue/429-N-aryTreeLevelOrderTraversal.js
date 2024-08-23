0b4c3b6ef0a647b568225a1c34e2b33552daa0e6
sherryjungwonzang <80478248+sherryjungwonzang@users.noreply.github.com>
2024-08-22 23:28:54 -0700

    Update and rename 429.-N-aryTreeLevelOrderTraversal.js to 429-N-aryTreeLevelOrderTraversal.js

--- LeetCode/Queue/429-N-aryTreeLevelOrderTraversal.js
+++ LeetCode/Queue/429-N-aryTreeLevelOrderTraversal.js
@@ -6,8 +6,9 @@
 //each group of children is separated by the null value
 
 //Approach:
-//BFS with queue
+//using BFS with queue
 var naryLevelOrderTraversal = (root) => {
+    //base case
     if (!root) return [];
 
     let queue = [root];
@@ -16,7 +17,7 @@ var naryLevelOrderTraversal = (root) => {
     //BFS
     while(queue.length) {
         let level = [];
-        let levelSize = queue.length; //when levelSize is 0 - push level into res
+        let levelSize = queue.length;
 
         while(levelSize) {
             let curr = queue.shift();
@@ -25,11 +26,15 @@ var naryLevelOrderTraversal = (root) => {
                 //if there are children
                 if (curr.children[i]) queue.push(curr.children[i]);
             }
+
             level.push(curr.val);
+            
             levelSize--;
         }
+
         res.push(level);
     }
+
     return res;
 }
 //TC: O(n)
@@ -38,6 +43,35 @@ naryLevelOrderTraversal([1,null,3,2,4,null,5,6]); //[[1], [3,2,4], [5,6]]
 //      1
 //  3    2  4
 //5  6
+
+//queue = [ [1, null, 3, 2, 4, null, 5, 6] ]
+//curr = [1, null, 3, 2, 4, null, 5, 6]
+//queue = [ [3, 5, 6], [2], [4] ]
+//level = [ 1 ]
+//res = [ [1], ]
+
+//queue = [ [3, 5, 6], [2], [4] ]
+//curr = [1, null, 3, 2, 4, null, 5, 6], [3, 5, 6]
+//queue = [ [2], [4] || [5], [6] ]
+//level = [ 3 ]
+
+//queue = [ [2], [4] || [5], [6] ]
+//curr = [1, null, 3, 2, 4, null, 5, 6], [3, 5, 6], [2]
+//level = [ 3 2 ]
+
+//queue = [ [4] || [5], [6] ]
+//curr = [1, null, 3, 2, 4, null, 5, 6], [3, 5, 6], [2], [4]
+//level = [ 3 2 4 ]
+//res = [ [1], [3, 2, 4] ]
+
+//queue = [ [5], [6] ]
+//curr = [1, null, 3, 2, 4, null, 5, 6], [3, 5, 6], [2], [4], [5]
+//level = [ 5 ]
+
+//queue = [ [6] ]
+//curr = [1, null, 3, 2, 4, null, 5, 6], [3, 5, 6], [2], [4], [5], [6]
+//level = [ 5 6 ]
+//res = [ [1], [3, 2, 4], [5, 6] ]
 
 naryLevelOrderTraversal([1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]); //[[1],[2,3,4,5],[6,7,8,9,10],[11,12,13],[14]]
 //           1
