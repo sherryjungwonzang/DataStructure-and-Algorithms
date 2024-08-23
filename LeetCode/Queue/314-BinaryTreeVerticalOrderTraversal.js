87fff9145059cb47e74e37c0ed17456c339e0b60
sherryjungwonzang <80478248+sherryjungwonzang@users.noreply.github.com>
2024-08-22 23:01:49 -0700

    Update 314-BinaryTreeVerticalOrderTraversal.js

--- LeetCode/Queue/314-BinaryTreeVerticalOrderTraversal.js
+++ LeetCode/Queue/314-BinaryTreeVerticalOrderTraversal.js
@@ -4,28 +4,27 @@
 //ex: from top to bottom, column by column
 
 //Approach:
-//using BFS with queue
+//using BFS with queue and map
 var verticalOrder = (root) => {
+    //base case
     if (!root) return [];
 
-    let queue = [[root, 0]]; //0 for the current level
+    let queue = [ [root, 0] ]; //0 for the current level
     let map = {}; //to store level information
 
     //BFS
     while(queue.length) {
-        const [node, level] = queue.shift();
+        let [node, level] = queue.shift(); //curr
 
-        if (!map[level]) {
-            //creating
-            map[level] = [node.val];
-        } else {
-            map[level].push(node.val);
-        }
+        //storing level information into map
+        if (!map[level]) map[level] = [node.val]; //creating
+        else map[level].push(node.val);
 
         //child node
         if (node.left) queue.push([node.left, level - 1]);
         if (node.right) queue.push([node.right, level + 1]);
     }
+
     //order the map by keys and return each values
     return Object.keys(map).sort((a, b) => a - b).map((k) => map[k]);
 }
@@ -37,35 +36,35 @@ verticalOrder([3,9,8,4,0,1,7]); //[[4], [9], [3,0,1], [8], [7]]
 //    4    0  1    7
 //in this tree, 3,0,1 is level 0 | 9 is level -1 | 4 is level -2 | 8 is level 1 | 7 is level 2
 
-//queue = [ [3, 0] ]
-//map = {}
-
-//shift off the [3, 0] & checking its child
-//queue = [ [9, -1], [8, 1] ]
+//queue = [ [[3,9,8,4,0,1,7], 0] ]
+//curr = [[3,9,8,4,0,1,7], 0]
 //map = { 0: [3] }
+//queue = [ [ [9, 4, 0], -1]: L, [ [8, 1, 7], 1]: R ]
 
-//shift off [9, -1] & checking its child
-//queue = [ [8, 1], [4, -2], [0, 0] ]
-//map = { 0: [3], -1: [9], }
+//queue = [ [ [9, 4, 0], -1], [ [8, 1, 7], 1] ]
+//curr = [[3,9,8,4,0,1,7], 0], [9, 4, 0], -1]
+//map = { 0: [3], -1: [9] }
+//queue = [ [ [8, 1, 7], 1] || [ [4], -2], [ [0], 0] ]
 
-//shift off [8, 1] & checking its child
-//queue = [ [4, -2], [0, 0], [1,0], [7,2] ]
+//queue = [ [ [8, 1, 7], 1] || [ [4], -2], [ [0], 0] ]
+//curr = [[3,9,8,4,0,1,7], 0], [9, 4, 0], -1], [8, 1, 7], 1]
 //map = { 0: [3], -1: [9], 1: [8] }
+//queue = [ [ [4], -2], [ [0], 0], [ [1], 0 ], [ [7], 2 ] ]
 
-//shift off [4, -2] & checking its child
-//queue = [ [0, 0], [1,0], [7,2] ]
+//queue = [ [ [4], -2], [ [0], 0], [ [1], 0 ], [ [7], 2 ] ]
+//curr = [[3,9,8,4,0,1,7], 0], [9, 4, 0], -1], [8, 1, 7], 1], [[4], -2]
 //map = { 0: [3], -1: [9], 1: [8], -2: [4] }
 
-//shift off [0, 0] & checking its child
-//queue = [ [1,0], [7,2] ]
+//queue = [ [ [0], 0], [ [1], 0 ], [ [7], 2 ] ]
+//curr = [[3,9,8,4,0,1,7], 0], [[9, 4, 0], -1], [8, 1, 7], 1], [[4], -2], [[0], 0]
 //map = { 0: [3, 0], -1: [9], 1: [8], -2: [4] }
 
-//shift off [1, 0] & checking its child
-//queue = [ [7,2] ]
+//queue = [ [ [1], 0 ], [ [7], 2 ] ]
+//curr = [ [3,9,8,4,0,1,7], 0 ], [ [9, 4, 0], -1 ], [ [8, 1, 7], 1 ], [ [4], -2 ], [ [0], 0 ], [ [1], 0 ]
 //map = { 0: [3, 0, 1], -1: [9], 1: [8], -2: [4] }
 
-//shift off [7, 2] & checking its child
-//queue = []
+//queue = [ [ [7], 2 ] ]
+//curr = [ [3,9,8,4,0,1,7], 0 ], [ [9, 4, 0], -1 ], [ [8, 1, 7], 1 ], [ [4], -2 ], [ [0], 0 ], [ [1], 0 ], [ [7], 2 ]
 //map = { 0: [3, 0, 1], -1: [9], 1: [8], -2: [4], 2: [7] }
 
 //sort by keys
