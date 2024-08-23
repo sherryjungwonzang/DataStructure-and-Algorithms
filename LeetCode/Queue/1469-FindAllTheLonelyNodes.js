2f8a3cb209580ca6174d0609f7835cbbbc8257e6
sherryjungwonzang <80478248+sherryjungwonzang@users.noreply.github.com>
2024-08-22 23:38:27 -0700

    Update 1469-FindAllTheLonelyNodes.js

--- LeetCode/Queue/1469-FindAllTheLonelyNodes.js
+++ LeetCode/Queue/1469-FindAllTheLonelyNodes.js
@@ -7,20 +7,21 @@
 //return the list in any order
 
 //Approach:
-//using BFS
+//using BFS with queue
 var findLonelyNodes_BFS = (root) => {
     //base case
     if (!root) return [];
 
     let queue = [root];
-    let res = [];;
+    let res = [];
     
+    //BFS
     while(queue.length) {
         let curr = queue.shift();
 
         //checking left side
         if (curr.left && !curr.right) {
-            queue.push(curr.legt);
+            queue.push(curr.left);
             res.push(curr.left.val);
         }
 
@@ -44,3 +45,21 @@ findLonelyNodes_BFS([1,2,3,null,4]); //[4] - node 4 at the bottom is the only lo
 //    1
 // 2    3
 //  4
+
+//queue = [ [1, 2, 3, null, 4] ]
+//curr = [1, 2, 3, null, 4]
+//queue = [ [2, null, 4], [3] ]
+
+//queue = [ [2, null, 4], [3] ]
+//curr = [1, 2, 3, null, 4], [2, null, 4]
+//only having right child
+//queue = [ [3] || [4] ]
+//res = [ 4 ]
+
+//queue = [ [4] ]
+//curr = [1, 2, 3, null, 4], [2, null, 4], [3]
+
+//queue = [ ]
+//curr = [1, 2, 3, null, 4], [2, null, 4], [3], [4]
+
+//res = [ 4 ]
