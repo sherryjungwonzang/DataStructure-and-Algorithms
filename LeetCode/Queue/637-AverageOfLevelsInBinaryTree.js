376754090e7609f73fc052cc615fcfeb5d1e37e5
sherryjungwonzang <80478248+sherryjungwonzang@users.noreply.github.com>
2024-08-24 12:27:46 -0700

    Update 637-AverageOfLevelsInBinaryTree.js

--- LeetCode/Queue/637-AverageOfLevelsInBinaryTree.js
+++ LeetCode/Queue/637-AverageOfLevelsInBinaryTree.js
@@ -3,7 +3,7 @@
 //return the average value of the nodes on each level in the form of an array
 
 //Approach:
-//using Queue with BFS
+//using BFS with queue
 var avgLevelsBinaryTree = (root) => {
     let avg = [];
     let queue = [root];
@@ -15,16 +15,47 @@ var avgLevelsBinaryTree = (root) => {
 
         for (let i = 0; i < levelSize; i++) {
             let curr = queue.shift();
-            sum += curr.val;
 
+            sum += curr.val; //sum of each level
+
+            //child nodes
             if (curr.left) queue.push(curr.left);
             if (curr.right) queue.push(curr.right);
         }
 
         avg.push(sum / levelSize);
     }
+    
     return avg;
 }
 avgLevelsBinaryTree([3,9,20,null,null,15,7]); //[3.00000,14.50000,11.00000]
+//    3
+//  9    20
+//     15  7
+
+//queue = [ [3,9,20,null,null,15,7] ]
+//curr = [3,9,20,null,null,15,7]
+//sum = 0 -> 3
+//queue = [ [9], [20, 15, 7] ]
+//avg = [(3 / 1) = 3.00000, ]
+
+//queue = [ [9], [20, 15, 7] ]
+//curr = [3,9,20,null,null,15,7], [9]
+//sum = 0 -> 9
+
+//queue = [ [20, 15, 7] ]
+//curr = [3,9,20,null,null,15,7], [9], [20, 15, 7] 
+//sum = 0 -> 9 -> 29
+//queue = [ [15], [7] ]
+//avg = [(3 / 1) = 3.00000, (29 / 2) = 14.50000, ]
+
+//queue = [ [15], [7] ]
+//curr = [3,9,20,null,null,15,7], [9], [20, 15, 7], [15] 
+//sum = 0 -> 15
+
+//queue = [ [7] ]
+//curr = [3,9,20,null,null,15,7], [9], [20, 15, 7], [15] , [7]
+//sum = 0 -> 15 -> 22
+//avg = [(3 / 1) = 3.00000, (29 / 2) = 14.50000, (22 / 2) = 11.00000]
 
 avgLevelsBinaryTree([3,9,20,15,7]); //[3.00000,14.50000,11.00000]
