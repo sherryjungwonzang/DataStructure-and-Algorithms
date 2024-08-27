ffa497f11689762693c67f39d49e1072610e00e3
sherryjungwonzang <80478248+sherryjungwonzang@users.noreply.github.com>
2024-08-27 16:57:45 -0700

    Update 127-WordLadder.js

--- LeetCode/Queue/127-WordLadder.js
+++ LeetCode/Queue/127-WordLadder.js
@@ -15,8 +15,9 @@ var wordLadder = (beginWord, endWord, wordList) => {
 
     //BFS
     while (queue.length) {
-        let [currWord, count] = queue.shift();
+        let [currWord, count] = queue.shift(); //curr
 
+        //base case
         if (currWord === endWord) return count;
 
         //checking all alphabet whether there is anything can be changed
@@ -25,22 +26,35 @@ var wordLadder = (beginWord, endWord, wordList) => {
                 //going through 'a' to 'z'
                 let letter = String.fromCharCode(97 + i); //String.fromCharCode(97) = 'a'
 
-                //making a new word from checkin a-z
+                //making a new word from checking a - z
                 let newWord = currWord.slice(0, j) + letter + currWord.slice(j + 1);
 
                 if (set.has(newWord)) {
                     queue.push([newWord, count + 1]);
+
                     set.delete(newWord); //for avoiding duplicate checkiong
                 }
             }
         }
     }
+
     return 0;
 }
 //TC: O(m^2 x n)
 //SC: O(m^2 x n)
 wordLadder(beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]); //5 
 //One shortest transformation sequence is "hit" -> "hot" -> "dot" -> "dog" -> cog", which is 5 words long
+
+//set = { "hot", "dot", "dog", "lot", "log", "cog" }
+//queue = [ ["hit", 1] ]
+//curr = ["hit", 1] 
+
+//i = 0, j = 0                       |i = 0, j = 1                        |i = 0, j = 2
+//                                  letter = fromCharCode(97) = 'a'
+//newWord = 'a' + 'it' = 'ait'       |newWord = 'h' + 'a' + 't' = 'hat'   |newWord = 'hi' + 'a' = 'hia'
+//...
+
+//queue = [ ["hit", 1], [hot, 2], [dot, 3], [log, 4], [dog, 5], [cog, 5] ]
 
 wordLadder(beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log"]); //0
 //The endWord "cog" is not in wordList, therefore there is no valid transformation sequence
