c8dd28ac19eeb889903079a6561713f32c7b6dfe
sherryjungwonzang <80478248+sherryjungwonzang@users.noreply.github.com>
2024-08-28 00:26:04 -0700

    Update 117-PopulatingNextRightPointersInEachNode2.js

--- LeetCode/Queue/117-PopulatingNextRightPointersInEachNode2.js
+++ LeetCode/Queue/117-PopulatingNextRightPointersInEachNode2.js
@@ -12,8 +12,7 @@
 //initially, all next pointers are set to NULL
 
 //Approach:
-//using queue with BFS
-//116 solution is also working for this question
+//using BFS with queue
 var populateNextRightPointer2 = (root) => {
     //base case
     if (!root) return root;
@@ -28,13 +27,63 @@ var populateNextRightPointer2 = (root) => {
             let curr = queue.shift();
 
             if (i + 1 < levelSize) curr.next = queue[0]; //meaning there is a right element
+
+            //child nodes
             if (curr.left) queue.push(curr.left);
             if (curr.right) queue.push(curr.right);
         }
     }
+
     return root;
 }
 populateNextRightPointer([1,2,3,4,5,null,7]); //[1,#,2,3,#,4,5,7,#]
 //      1
 //    2   3
 //  4  5    7 
+
+//levelSize = 1
+//queue = [ [1, 2, 3, 4, 5, null, 7] ]
+//curr = [1, 2, 3, 4, 5, null, 7] 
+//i = 0
+//0 + 1 = 1 < 1 -> NO - meaning there is no node next to 1
+//keeping next as NULL
+//queue = [ [2, 4, 5], [3, null, 7] ]
+
+//levelSize = 2
+//queue = [ [2, 4, 5], [3, null, 7] ]
+//curr = [1, 2, 3, 4, 5, 6, 7], [2, 4, 5]
+//i = 0
+//0 + 1 = 1 < 2 -> YES - meaning there is right node next to 2
+//curr.next = [3]
+//queue = [ [3, null, 7] || [4], [5] ]
+
+//queue = [ [3, null, 7] || [4], [5] ]
+//curr = [1, 2, 3, 4, 5, 6, 7], [2, 4, 5], [3, null, 7]
+//i = 1
+//1 + 1 = 1 = 2 -> NO - meaning there is no node next to 3
+//keeping next as NULL
+//queue = [ [4], [5], [7] ]
+
+//levelSize = 3
+//queue = [ [4], [5], [7] ]
+//curr = [1, 2, 3, 4, 5, 6, 7], [2, 4, 5], [3, null, 7], [4]
+//i = 0
+//0 + 1 = 1 < 3 -> YES - meaning there is right node next to 4
+//curr.next = [5]
+//queue = [ [5], [7] ]
+
+//queue = [ [5], [7] ]
+//curr = [1, 2, 3, 4, 5, 6, 7], [2, 4, 5], [3, null, 7], [4], [5]
+//i = 1
+//1 + 1 = 2 < 3 -> YES - meaning there is right node next to 5
+//curr.next = [7]
+//queue = [ [7] ]
+
+//queue = [ [7] ]
+//curr = [1, 2, 3, 4, 5, 6, 7], [2, 4, 5], [3, null, 7], [4], [5], [7]
+//i = 2
+//2 + 1 = 3 = 3 -> NO - meaning there is no node next to 7
+//keeping next as NULL
+//queue = [ ]
+
+//[1, #, 2, 3, #, 4, 5, 7, #]
