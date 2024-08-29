5f63386dc2ba0333e27cb6907add56df84ffe771
sherryjungwonzang <80478248+sherryjungwonzang@users.noreply.github.com>
2024-08-29 13:28:04 -0700

    Update 1926-NearestExitFromEntranceInMaze.js

--- LeetCode/Queue/1926-NearestExitFromEntranceInMaze.js
+++ LeetCode/Queue/1926-NearestExitFromEntranceInMaze.js
@@ -27,23 +27,28 @@ var nearestExit = (maze, entrance) => {
         return (row === 0 || col === 0 || row === m - 1 || col === n - 1) && maze[row][col] === '.';
     }
 
+    //creating visited array
     let visited = [];
+
     for (let i = 0; i < m; i++) {
         visited.push(new Array(maze[0].length).fill(false));
     }
+    
     visited[entrance[0]][entrance[1]] = true; //marking true as visited entrance
 
-    let queue = [entrance];
+
+    //BFS
+    let queue = [ entrance ];
     let dirs = [ [0, 1], [1, 0], [-1, 0], [0, -1] ];
     let steps = 0;
 
-    //BFS
     while (queue.length) {
         let next = [];
+
         steps++; //entrance is not an exit but steps
 
         for (let i = 0; i < queue.length; i++) {
-            let [row, col] = queue[i]; //same function as 'curr' in BFS
+            let [row, col] = queue[i]; //curr
 
             //next row and col
             for (let [dx, dy] of dirs) {
@@ -54,17 +59,72 @@ var nearestExit = (maze, entrance) => {
                     if (isExit(nextRow, nextCol)) return steps;
                     else {
                         visited[nextRow][nextCol] = true;
+
                         next.push([nextRow, nextCol]);
                     }
                 }
             }
         }
+
         queue = next; //updating queue with nextqueue
     }
+    
     return -1;
 }
 nearestExit(maze = [["+","+",".","+"],[".",".",".","+"],["+","+","+","."]], entrance = [1,2]); //1 - [0, 2] is the nearest exit
+// +  +  .  +
+// .  .  .  +
+// +  +  +  .
+
+//visited = [ F, F, F, F ]
+//          [ F, F, T, F ]
+//          [ F, F, F, F ]
+
+//steps = 0
+//queue = [ [1, 2] ]
+//curr = [1, 2]
+//steps = 0 -> 1
+
+//nextR               nextC
+//0 + 1 = 1           1 + 2 = 3 -> +
+//1 + 1 = 2           0 + 2 = 2 -> +
+//-1 + 1 = 0          0 + 2 = 2 -> . & row = 0: is Exit
+//0 + 1 = 1           -1 + 2 = 1 -> .
+//return steps = 1
 
 nearestExit(maze = [["+","+","+"],[".",".","."],["+","+","+"]], entrance = [1,0]); //2 - [1, 2] is the nearest exit
+// +  +  +
+// .  .  .
+// +  +  +
+
+//visited = [ F, F, F ]
+//          [ T, F, F ]
+//          [ F, F, F ]
+
+//steps = 0
+//queue = [ [1, 0] ]
+//curr = [1, 0]
+//steps = 0 -> 1
+
+//nextR               nextC
+//0 + 1 = 1           1 + 0 = 1 -> .
+//1 + 1 = 2           0 + 0 = 0 -> +
+//-1 + 1 = 0          0 + 0 = 0 -> +
+//0 + 1 = 1           -1 + 0 = -1 -> invalid
+//next = [1, 1]
+//visited = [ F, F, F ]
+//          [ T, T, F ]
+//          [ F, F, F ]
+
+//queue = [ [1, 1] ]
+//curr = [1, 0], [1, 1]
+//steps = 0 -> 1 -> 2
+
+//nextR               nextC
+//0 + 1 = 1           1 + 1 = 2 -> . & nextC = n - 1: is Exit
+//1 + 1 = 2           0 + 1 = 1 -> +
+//-1 + 1 = 0          0 + 1 = 1 -> +
+//0 + 1 = 1           -1 + 1 = 0 -> entrance
+//return steps = 2
 
 nearestExit(maze = [[".","+"]], entrance = [0,0]); //-1 - there are no exits in thie maze
