c67488b31c5661b3be0e3098c7e19ddc3dcd9c75
sherryjungwonzang <80478248+sherryjungwonzang@users.noreply.github.com>
2024-09-01 04:02:11 -0700

    Update 721-AccountsMerge.js

--- LeetCode/Graph/721-AccountsMerge.js
+++ LeetCode/Graph/721-AccountsMerge.js
@@ -18,18 +18,28 @@ var accountsMerge = (accounts) => {
     let visited = new Set();
     let res = [];
 
-    //creating a GRAPH and setting adjList
     for (let i = 0; i < accounts.length; i++) {
-        for (let j = 2; j < accounts[i].length; j++) {
-            let arr1 = adjList.get(accounts[i][1]) || [];
-            arr1.push(accounts[i][j]);
-            adjList.set(accounts[i][1], arr1); //setting adjList
+        for (let j = 2; j < accounts[i].length; j++) { //limited to person who has two emails
+            let firstToSecond = adjList.get(accounts[i][1]) || []; //check first email whether there is or not
+            firstToSecond.push(accounts[i][j]); //add second email
+            adjList.set(accounts[i][1], firstToSecond); //connecting each email address - (first, second)
 
-            let arr2 = adjList.get(accounts[i][j]) || [];
-            arr2.push(accounts[i][1]);
-            adjList.set(accounts[i][j], arr2);
-        }
+            let secondToFirst = adjList.get(accounts[i][j]) || []; //check second email whether there is or not
+            secondToFirst.push(accounts[i][1]); //add first email
+            adjList.set(accounts[i][j], secondToFirst); //connecting each email address - (second, first)
+        } 
     };
+
+    for (let i = 0; i < accounts.length; i++) {
+        //based on first email
+        if (!visited.has(accounts[i][1])) {
+            let result = dfs(accounts[i][1], []);
+
+            result.sort(); //sorting
+
+            res.push([accounts[i][0], ...result]); //matching with name and emails
+        }
+    }
 
     //DFS
     function dfs(email, result) {
@@ -41,24 +51,96 @@ var accountsMerge = (accounts) => {
         for (neigh of neighbors) {
             if (!visited.has(neigh)) dfs(neigh, result);
         }
+        
         return result;
     };
 
-    for (let i = 0; i < accounts.length; i++) {
-        if (!visited.has(accounts[i][1])) {
-            let result = dfs(accounts[i][1], []);
 
-            result.sort(); //sorting
-            res.push([accounts[i][0], ...result]); //matching with name and emails
-        }
-    }
     return res;
 }
 accountsMerge([["John","johnsmith@mail.com","john_newyork@mail.com"],["John","johnsmith@mail.com","john00@mail.com"],["Mary","mary@mail.com"],["John","johnnybravo@mail.com"]]); 
-//[["John","john00@mail.com","john_newyork@mail.com","johnsmith@mail.com"],["Mary","mary@mail.com"],["John","johnnybravo@mail.com"]]
-//The first and second John's are the same person as they have the common email "johnsmith@mail.com".
-//The third John and Mary are different people as none of their email addresses are used by other accounts.
-//We could return these lists in any order, for example the answer [['Mary', 'mary@mail.com'], ['John', 'johnnybravo@mail.com'], ['John', 'john00@mail.com', 'john_newyork@mail.com', 'johnsmith@mail.com']] would still be accepted
+//["John","johnsmith@mail.com","john_newyork@mail.com"]
+//["John","johnsmith@mail.com","john00@mail.com"]
+//["Mary","mary@mail.com"]
+//["John","johnnybravo@mail.com"]
+
+//i = 0, j = 2 
+//[0][1] = "johnsmith@mail.com" -> not in adjList
+//push [0][2] -> firstToSecond = [ "john_newyork@mail.com" ]
+//adjList = { "johnsmith@mail.com" :  "john_newyork@mail.com" }
+//---------------------------------------------------------------------
+//[0][2] = "john_newyork@mail.com" -> not in adjList
+//push [0][1] -> secondToFirst = [ "johnsmith@mail.com" ]
+//adjList = { "johnsmith@mail.com" :  "john_newyork@mail.com" 
+//           "john_newyork@mail.com" : "johnsmith@mail.com"
+//}
+
+//i = 1, j = 2 
+//[1][1] = "johnsmith@mail.com" -> in adjList
+//push [1][2] -> firstToSecond = [ "john_newyork@mail.com", "john00@mail.com" ]
+//adjList = { "johnsmith@mail.com" :  "john_newyork@mail.com", "john00@mail.com"
+//           "john_newyork@mail.com" : "johnsmith@mail.com"
+//}
+//---------------------------------------------------------------------
+//[1][2] = "john00@mail.com" -> not in adjList
+//push [1][1] -> secondToFirst = [ "johnsmith@mail.com" ]
+//adjList = { "johnsmith@mail.com" :  [ "john_newyork@mail.com", "john00@mail.com" ]
+//           "john_newyork@mail.com" : [ "johnsmith@mail.com" ]
+//           "john00@mail.com" : [ "johnsmith@mail.com" ]
+//}
+
+
+//adjList = { "johnsmith@mail.com" :  [ "john_newyork@mail.com", "john00@mail.com" ]
+//           "john_newyork@mail.com" : [ "johnsmith@mail.com" ]
+//           "john00@mail.com" : [ "johnsmith@mail.com" ]
+//}
+
+//i = 0 -> [0][1] = "johnsmith@mail.com"
+//visited = { }
+//result = dfs("johnsmith@mail.com", [])
+
+//starting with dfs("johnsmith@mail.com", [])
+//visited = { "johnsmith@mail.com" }
+//result = { "johnsmith@mail.com" }
+//neighbor: "john_newyork@mail.com" -> dfs("john_newyork@mail.com", "johnsmith@mail.com")
+//          "john00@mail.com" -> dfs("john00@mail.com", "johnsmith@mail.com")
+
+//dfs("john_newyork@mail.com", "johnsmith@mail.com")
+//visited = { "johnsmith@mail.com" , "john_newyork@mail.com" }
+//result = { "johnsmith@mail.com" , "john_newyork@mail.com"  }
+//neighbor: "johnsmith@mail.com" -> aleady in visited
+
+//dfs("john00@mail.com", "johnsmith@mail.com")
+//visited = { "johnsmith@mail.com" , "john_newyork@mail.com", "john00@mail.com" }
+//result = { "johnsmith@mail.com" , "john_newyork@mail.com", "john00@mail.com" }
+//neighbor: "johnsmith@mail.com" -> aleady in visited
+
+//dfs("johnsmith@mail.com", []) -> return = [ "johnsmith@mail.com" , "john_newyork@mail.com", "john00@mail.com" ]
+//-> res = ['John'|| "johnsmith@mail.com" , "john_newyork@mail.com", "john00@mail.com" ]
+//-> sorting: ['John'|| "john00@mail.com", "john_newyork@mail.com", "johnsmith@mail.com" ]
+
+//i = 1 -> [1][1] = "johnsmith@mail.com" -> visited has
+
+//i = 2 -> [2][1] = "mary@mail.com"
+//dfs("mary@mail.com", [])
+//visited = { "johnsmith@mail.com" , "john_newyork@mail.com", "john00@mail.com", "mary@mail.com" }
+//result = { "johnsmith@mail.com" , "john_newyork@mail.com", "john00@mail.com", "mary@mail.com" }
+//neighbor: []
+//dfs("mary@mail.com", []) -> return = [ "mary@mail.com" ]
+//-> res = ['John'|| "john00@mail.com", "john_newyork@mail.com", "johnsmith@mail.com", 
+//          'Mary' || "mary@mail.com",
+//]
+
+//i = 3 -> [3][1] = "johnnybravo@mail.com"
+//dfs("johnnybravo@mail.com" [])
+//visited = { "johnsmith@mail.com" , "john_newyork@mail.com", "john00@mail.com", "mary@mail.com", "johnnybravo@mail.com" }
+//result = { "johnsmith@mail.com" , "john_newyork@mail.com", "john00@mail.com", "mary@mail.com", "johnnybravo@mail.com" }
+//neighbor: []
+//dfs("johnnybravo@mail.com" []) -> return = [ "johnnybravo@mail.com"]
+//-> res = ['John'|| "john00@mail.com", "john_newyork@mail.com", "johnsmith@mail.com"
+//          'Mary' || "mary@mail.com",
+//          'John' || "johnnybravo@mail.com"
+//]
 
 accountsMerge([["Gabe","Gabe0@m.co","Gabe3@m.co","Gabe1@m.co"],["Kevin","Kevin3@m.co","Kevin5@m.co","Kevin0@m.co"],["Ethan","Ethan5@m.co","Ethan4@m.co","Ethan0@m.co"],["Hanzo","Hanzo3@m.co","Hanzo1@m.co","Hanzo0@m.co"],["Fern","Fern5@m.co","Fern1@m.co","Fern0@m.co"]]); 
 //[["Ethan","Ethan0@m.co","Ethan4@m.co","Ethan5@m.co"],["Gabe","Gabe0@m.co","Gabe1@m.co","Gabe3@m.co"],["Hanzo","Hanzo0@m.co","Hanzo1@m.co","Hanzo3@m.co"],["Kevin","Kevin0@m.co","Kevin3@m.co","Kevin5@m.co"],["Fern","Fern0@m.co","Fern1@m.co","Fern5@m.co"]]
