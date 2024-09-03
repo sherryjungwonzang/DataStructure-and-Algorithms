0826c8ab84ff4c3f2f53ff397db089435af05d3c
sherryjungwonzang <80478248+sherryjungwonzang@users.noreply.github.com>
2024-09-03 03:00:05 -0700

    Update 530-MinimumAbsoluteDifferenceInBST.js

--- LeetCode/Tree/530-MinimumAbsoluteDifferenceInBST.js
+++ LeetCode/Tree/530-MinimumAbsoluteDifferenceInBST.js
@@ -8,9 +8,9 @@ var minDifferenceBST = (root) => {
     let prev = Infinity;
     let minDiff = Infinity;
 
-    //inorder traversal
+    //inorder traversal: left - root - right
     function inorder(root) {
-        //left - root - right
+        //base case
         if (root.left) inorder(root.left);
 
         minDiff = Math.min(minDiff, Math.abs(root.val - prev));
@@ -36,23 +36,36 @@ minDifferenceBST([4, 2, 6, 1, 3]); //1
 
 //starting with inorder([4, 2, 6, 1, 3])
 //Left: inorder([2, 1, 3])
-//minDiff = Infinity ->  min(Infinity, abs(4 - Infinity)) = Infinity
-//prev = Infinity -> 4
 //Right: inorder([6])
 
 //inorder([2, 1, 3])
 //Left: inorder([1])
-//minDiff = Infinity -> Infinity -> min(Infinity, abs(2 - 4)) = 2
-//prev = Infinity -> 4 -> 2
+//Right: inorder([3])
+
+//Left first - inorder([1])
+//minDiff = Infinity -> min(Infi, abs(1 - Infi)) = Infi
+//prev = Infinity -> 1
+
+//back to inorder([2, 1, 3])
+//Left: inorder([1])
+//minDiff = Infinity -> min(Infi, abs(1 - Infi)) = Infi -> (Infi, abs(2 - 1)) = 1
+//prev = Infinity -> 1 -> 2
 //Right: inorder([3])
 
 //inorder([3])
-//minDiff = Infinity -> Infinity -> 2 -> min(2, abs(3 - 2)) = 1
-//prev = Infinity -> 4 -> 3
+//minDiff = Infinity -> min(Infi, abs(1 - Infi)) = Infi -> (Infi, abs(2 - 1)) = 1 -> (1, abs(3 - 2)) = 1
+//prev = Infinity -> 1 -> 2 -> 3
+
+//back to inorder([4, 2, 6, 1, 3])
+//Left: inorder([2, 1, 3])
+//minDiff = Infinity -> min(Infi, abs(1 - Infi)) = Infi -> (Infi, abs(2 - 1)) = 1 -> (1, abs(3 - 2)) = 1 -> (1, abs(4 - 3)) = 1
+//prev = Infinity -> 1 -> 2 -> 3 -> 4
+//Right: inorder([6])
 
 //inorder([6])
-//minDiff = Infinity -> Infinity -> 2 -> 1 -> min(1, abs(6 - 3)) = 1
-//prev = Infinity -> 4 -> 3 -> 1
+//minDiff = Infinity -> min(Infi, abs(1 - Infi)) = Infi -> (Infi, abs(2 - 1)) = 1 -> (1, abs(3 - 2)) = 1 -> (1, abs(4 - 3)) = 1 -> (1, abs(6 - 4)) = 1
+//prev = Infinity -> 1 -> 2 -> 3 -> 4 -> 6
+
 
 minDifferenceBST([1, 0, 48, null, null, 12, 49]); //1
 //    1
@@ -64,29 +77,28 @@ minDifferenceBST([1, 0, 48, null, null, 12, 49]); //1
 
 //starting with inorder([1, 0, 48, null, null, 12, 49])
 //Left: inorder([0])
-//minDiff = Infinity ->  min(Infinity, abs(1 - Infinity)) = Infinity
-//prev = Infinity -> 0
 //Right: inorder([48, 12, 49])
 
-//inorder([0])
-//minDiff = Infinity -> Infinity ->  min(Infinity, abs(0 - 1)) = 1
-//prev = Infinity -> 1 -> 0
+//Left first - inorder([0])
+//minDiff = Infinity -> min(Infi, abs(0 - Infi)) = Infi
+//prev = Infinity -> 0
+
+//back to inorder([1, 0, 48, null, null, 12, 49])
+//Left: inorder([0])
+//minDiff = Infinity -> min(Infi, abs(0 - Infi)) = Infi -> (Infi, abs(1 - 0)) = 1
+//prev = Infinity -> 0 -> 1
+//Right: inorder([48, 12, 49])
+
+//inorder([12])
+//minDiff = Infinity -> min(Infi, abs(0 - Infi)) = Infi -> (Infi, abs(1 - 0)) = 1 -> (1, abs(12 - 1)) = 1
+//prev = Infinity -> 0 -> 1 -> 12
 
 //inorder([48, 12, 49])
 //Left: inorder([12])
-//minDiff = Infinity -> Infinity -> 1 -> min(1, abs(48 - 0)) = 1
-//prev = Infinity -> 1 -> 0 -> 48
+//minDiff = Infinity -> min(Infi, abs(0 - Infi)) = Infi -> (Infi, abs(1 - 0)) = 1 -> (1, abs(12 - 1)) = 1 -> (1, abs(48 - 12)) = 1
+//prev = Infinity -> 0 -> 1 -> 12 -> 48
 //Right: inorder([49])
 
-//inorder([12])
-//minDiff = Infinity -> Infinity -> 1 -> 1 -> min(1, abs(12 - 48)) = 1
-//prev = Infinity -> 1 -> 0 -> 48 -> 12
-
 //inorder([49])
-//minDiff = Infinity -> Infinity -> 1 -> 1 -> 1 -> min(1, abs(49 - 12)) = 1
-//prev = Infinity -> 1 -> 0 -> 48 -> 12 -> 49
-
-
-
-
-
+//minDiff = Infinity -> min(Infi, abs(0 - Infi)) = Infi -> (Infi, abs(1 - 0)) = 1 -> (1, abs(12 - 1)) = 1 -> (1, abs(48 - 12)) = 1 -> (1, abs(49 - 48)) = 1
+//prev = Infinity -> 0 -> 1 -> 12 -> 48 -> 49
