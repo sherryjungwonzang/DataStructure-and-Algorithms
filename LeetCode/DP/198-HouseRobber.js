3a022af6d9a406b9e4e5cf3e6b5e66006b663a0c
sherryjungwonzang <80478248+sherryjungwonzang@users.noreply.github.com>
2024-09-04 02:06:51 -0700

    Update 198-HouseRobber.js

--- LeetCode/DP/198-HouseRobber.js
+++ LeetCode/DP/198-HouseRobber.js
@@ -8,51 +8,46 @@
 //and it will automatically contact the police if two adjacent houses were broken into on the same night
 
 //Approach:
-//DP with bottom approach - utilize the smallest kind of solution to work out larger solutions
+//using DP
 var houseRobber = (nums) => {
-  if (nums.length === 0) return 0; //not stealing anything if there is no houses
-  if (nums.length === 1) return nums[0];
+    //base case
+    if (nums.length === 0) return 0; //no stealing
+    if (nums.length === 1) return nums[0]; //only one house to steal
 
-  //DP array
-  let dp = Array(nums + 1).fill(0);
+    //DP
+    let dp = Array(nums).fill(0);
 
-  //base cases of DP Array
-  dp[0] = nums[0];
-  dp[1] = Math.max(nums[0], nums[1]);
+    dp[0] = nums[0];
+    dp[1] = Math.max(nums[0], nums[1]);
 
-  //recursive calls
-  for (let i = 2; i < nums.length; i++) {
-    dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
-  }
-  return dp[dp.length - 1];
+    
+    for (let i = 2; i < nums.length; i++) {
+        dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+    }
+
+    return dp[dp.length - 1];
 }
 //TC: O (n) - we carried out dp[0] and dp[1]
 //SC: O (n) - allocate at DP Array same size with nums
 houseRobber([1,2,3,1]); //4 - Rob house 1| money=1 and then rob house 3 | money=3 - total 1+3=4
-//nums = [1, 2, 3, 1]
-//DP Array - filling with 0
-//  0  1  2  3
-//  0  0  0  0
-
+//[1, 2, 3, 1]
+//DP = [ 0  0  0  0 ] -> [ 1  2  4  4 ] 
 //dp[0] = nums[0] = 1
 //dp[1] = max(nums[0], nums[1]) = max(1, 2) = 2
-//dp[2] = max(nums[2] + dp[0], dp[1]) = max(3+1, 2) = 4
-//dp[3] = max(nums[3] + dp[1], dp[2]) = max(1+2, 4) = 4
 
-//  0  1  2  3
-//  1  2  4  4 -> return value
+//[1, 2, 3, 1]
+//       ^
+//dp[2] = max(dp[1], dp[0] + nums[2]) = max(2, 3 + 1) = 4
+//dp[3] = max(dp[2], dp[1] + nums[3]) = max(4, 1 + 2) = 4
 
 houseRobber([2,7,9,3,1]); //12 - Rob house 1 | money=2 and rob house 3 | money=9 and rob house 5 | money=1 - total amount you can do rob is 2+9+1=12
-//nums = [2, 7, 9, 3, 1]
-//DP Array - filling with 0
-//  0  1  2  3  4
-//  0  0  0  0  0
-
+//[2, 7, 9, 3, 1]
+//DP = [ 0  0  0  0  0 ] -> [ 2  7  11  11  12 ]
 //dp[0] = nums[0] = 2
 //dp[1] = max(nums[0], nums[1]) = max(2,7) = 7
-//dp[2] = max(nums[2] + dp[0], dp[1]) = max(9+2, 7) = 11
-//dp[3] = max(nums[3] + dp[1], dp[2]) = max(3+7, 11) = 11
-//dp[4] = max(nums[4] + dp[2], dp[3]) = max(1+11, 11) = 12
 
-//  0  1  2  3  4
-//  2  7  11 11 12    -> return value
+//[2, 7, 9, 3, 1]
+//       ^
+//dp[2] = max(dp[1], dp[0] + nums[2]) = max(7, 9 + 2) = 11
+//dp[3] = max(dp[2], dp[1] + nums[3]) = max(11, 3 + 7) = 11
+//dp[4] = max(dp[3], dp[2] + nums[4]) = max(11, 1 + 11) = 12
