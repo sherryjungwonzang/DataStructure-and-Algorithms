aa31910c468d731432f4d442d4f64addcab6e54e
sherryjungwonzang <80478248+sherryjungwonzang@users.noreply.github.com>
2024-09-04 21:21:21 -0700

    Update 63-UniquePaths2.js

--- LeetCode/DP/63-UniquePaths2.js
+++ LeetCode/DP/63-UniquePaths2.js
@@ -4,24 +4,23 @@
 //the robot tried to move to the bottom-right corner - grid[m-1][n-1]
 //the robot can only move either down or right at any point in time
 
-//an obstacle and space are makred as 1 or 0 respectively in grid
+//an obstacle and space are marked as 1 or 0 respectively in grid
 //a path that the robot takes cannot include any square that is an obstacle
 
 //return the number of possible unique paths that the robot can take to reach the bottom-right corner
 //the testcases are generated so that the answer will be less than or equal to 2 * 10^9
 
 //Approach:
-//using DP - upon sub-problems
+//using DP
 var uniquePaths2 = (obstacleGrid) => {
-    let  m = obstacleGrid.length; //row
-    let  n = obstacleGrid[0].length; //col
+    let m = obstacleGrid.length; //row
+    let n = obstacleGrid[0].length; //col
     let dp = Array.from(Array(m),  () => Array(n).fill(0));
 
     //looping through rows
     for (let i = 0; i < m; i++) {
-        //checking value in the cell
-        if (obstacleGrid[i][0] === 1) { 
-            dp[i][0] = 0; //obstacle in the grid
+        if (obstacleGrid[i][0] === 1) { //obstacle in the grid
+            dp[i][0] = 0; 
             break;
         } else { //no obstacle
             dp[i][0] = 1;
@@ -29,12 +28,12 @@ var uniquePaths2 = (obstacleGrid) => {
     };
 
     //looping through cols
-    for (let j = 0; j < n; j++) { 
-        if (obstacleGrid[0][j] === 1) {
-            dp[0][j] = 0;
+    for (let i = 0; i < n; i++) { 
+        if (obstacleGrid[0][i] === 1) {
+            dp[0][i] = 0;
             break;
         } else {
-            dp[0][j] = 1;
+            dp[0][i] = 1;
         }
     };
 
@@ -50,32 +49,30 @@ var uniquePaths2 = (obstacleGrid) => {
 }
 uniquePaths2([[0,0,0], [0,1,0], [0,0,0]]); //2 - There is one obstacle in the middle of the 3x3 grid above.
 //There are two ways to reach the bottom-right corner: 1. Right -> Right -> Down -> Down, 2. Down -> Down -> Right -> Right
-// Grid                                        DP  
-// 0 0 0                                     0 0 0 
-// 0 1 0 -> there is an obstacle             0 0 0 
-// 0 0 0                                     0 0 0
+
+// 0 0 0 
+// 0 1 0 -> there is an obstacle 
+// 0 0 0                        
 
 //checking row first
-// DP 
-// 1 0 0 
-// 1 0 0
-// 1 0 0
-//no obstacle -> change 0 to 1 in DP array
+// DP - [ 0, 0, 0 ]     [ 1, 0, 0 ]
+//      [ 0, 0, 0 ] ->  [ 1, 0, 0 ]
+//      [ 0, 0, 0 ]     [ 1, 0, 0 ]
+//no obstacle in row -> change 0 to 1 in DP array
 
 //checking column 
-// DP 
-// 1 1 1 
-// 1 0 0
-// 1 0 0
-//no obstacle -> change 0 to 1 in DP array
+// DP - [ 1, 0, 0 ]     [ 1, 1, 1 ]
+//      [ 1, 0, 0 ] ->  [ 1, 0, 0 ]
+//      [ 1, 0, 0 ]     [ 1, 0, 0 ]
+//no obstacle in col -> change 0 to 1 in DP array
 
-//checking the rest of Grid -> (1, 1): obstacle
-// DP 
-// 1 1 1 
-// 1 0 1 -> obstacle(1,1): leave it as 0
-// 1 1 2
-//(1,2): 0[0,2] + 1[1,1] = 1
-//(2,1): 0[1,1] + 1[2,0] = 1
-//(2,2): 1[2,1] + 1[1,2] = 2
+//checking the rest of Grid -> (1, 1) in grid: obstacle -> change to 0 in DP
+// DP - [ 1, 1, 1 ]
+//      [ 1, 0, 1 ]
+//      [ 1, 1, 2 ]
+//i = 1, j = 1 in grid = obstalce -> dp[1][1] = 0
+//i = 1, j = 2 in grid not obstacle -> dp[1][2] = dp[0][1] + dp[1][1] = 1 + 0 = 1  
+//i = 2, j = 1 in grid not obstacle -> dp[2][1] = dp[1][1] + dp[2][0] = 0 + 1 = 1  
+//i = 2, j = 2 in grid not obstacle -> dp[2][2] = dp[1][2] + dp[2][1] = 1 + 1 = 2
 
 uniquePaths2([[0,1], [0,0]]); //1
