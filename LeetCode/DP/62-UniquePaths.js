e74408167c4bdc2823b3775e4488fbb40c9c6aa5
sherryjungwonzang <80478248+sherryjungwonzang@users.noreply.github.com>
2024-09-04 17:58:21 -0700

    Update 62-UniquePaths.js

--- LeetCode/DP/62-UniquePaths.js
+++ LeetCode/DP/62-UniquePaths.js
@@ -1,5 +1,5 @@
 //62. Unique Paths
-//given the two integers'm' and 'n'
+//given the two integers 'm' and 'n'
 //return the num of possible unique paths that robot can take to reach the bottom-right corner
 
 //there is a robot on an 'm x n' grid
@@ -8,41 +8,43 @@
 //the robot can only move either down or right at any point in time
 
 //Approach:
-//using 2D Array
-//setting the first row and column as 1 - there is only one way of getting to that position
-var uniquePaths = (m, n) => {
+//using 2D DP array
+var uniquePaths = (a, b) => {
     let dp = Array.from(Array(m), () => new Array(n));  //2D DP array
-    let x = dp.length;
-    let y = dp[0].length;
+    let m = dp.length;
+    let n = dp[0].length;
 
     //looping first row and col with setting as 1 - only one way of getting to that position
-    for (let i = 0; i < x; i++) dp[i][0] = 1;
-    for (let i = 0; i < y; i++) dp[0][i] = 1;
+    for (let i = 0; i < m; i++) dp[i][0] = 1;
+    for (let i = 0; i < n; i++) dp[0][i] = 1;
 
     //the rest of DP
-    for (let i = 1; i < x; i++) {
-        for (let j = 1; j < y; j++) {
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
             dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
         }
     }
 
-    return dp[m - 1][n - 1];
+    return dp[a - 1][b - 1];
 }
 //TC: O(m * n) - traversing every single value within the grid
 //SC: O(m * n) - storing new values within DP Array
 uniquePaths(3, 7); //28
 //DP:
-// 0 | 1 | 1 | 1  | 1  | 1  | 1 |
-// 1 | 2 | 3 | 4  | 5  | 6  | 7 |
-// 1 | 3 | 6 | 10 | 15 | 21 | 28 |
+// [  ,  ,  ,  ,  ,  ,  ]            [ 1, 1, 1, 1, 1, 1, 1]
+// [  ,  ,  ,  ,  ,  ,  ]     ->     [ 1, 2, 3, 4, 5, 6, 7]
+// [  ,  ,  ,  ,  ,  ,  ]            [ 1, 3, 6, 10, 15, 21, 28]
 
-uniquePaths(3, 2); //3
-//explanation: from the top-left corner, there are total of 3 ways to each the bottom-right corner
-//right -> down -> down
-//down -> down -> right
-//down -> right -> down
- 
-//DP:
-// 0 | 1 |
-// 1 | 2 |
-// 1 | 3 |
+//i = 1, j = 1 -> dp[1][1] = dp[0][1] + dp[1][0] = 1 + 1 = 2
+//i = 1, j = 2 -> dp[1][2] = dp[0][2] + dp[1][1] = 1 + 2 = 3
+//i = 1, j = 3 -> dp[1][3] = dp[0][3] + dp[1][2] = 1 + 3 = 4
+//i = 1, j = 4 -> dp[1][4] = dp[0][4] + dp[1][3] = 1 + 4 = 5
+//i = 1, j = 5 -> dp[1][5] = dp[0][5] + dp[1][4] = 1 + 5 = 6
+//i = 1, j = 6 -> dp[1][6] = dp[0][6] + dp[1][5] = 1 + 6 = 7
+
+//i = 2, j = 1 -> dp[2][1] = dp[1][1] + dp[2][0] = 2 + 1 = 3
+//i = 2, j = 2 -> dp[2][2] = dp[1][2] + dp[2][1] = 3 + 3 = 6
+//i = 2, j = 3 -> dp[2][3] = dp[1][3] + dp[2][2] = 4 + 6 = 10
+//i = 2, j = 4 -> dp[2][4] = dp[1][4] + dp[2][3] = 5 + 10 = 15
+//i = 2, j = 5 -> dp[2][5] = dp[1][5] + dp[2][4] = 6 + 15 = 21
+//i = 2, j = 6 -> dp[2][6] = dp[1][6] + dp[2][5] = 7 + 21 = 28
