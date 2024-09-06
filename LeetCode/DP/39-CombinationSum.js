4e565ae167f7137b92779923012b6f046b07d1c6
sherryjungwonzang <80478248+sherryjungwonzang@users.noreply.github.com>
2024-09-06 03:33:01 -0700

    Update 39-CombinationSum.js

--- LeetCode/DP/39-CombinationSum.js
+++ LeetCode/DP/39-CombinationSum.js
@@ -1,4 +1,4 @@
-//39. Combination Sum
+//39. Combination Sum (DFS solution is better - adding here just in case)
 //given an array of distinct integers 'candidates' and a target integer 'target'
 //return a list of all unique combination of candidates - where the chosen numbers sum to target
 //may return the combincations in any order
@@ -9,70 +9,133 @@
 //it is guaranteed that the num of unique combinations that sum up to target is less than 150 combinations for the given input
 
 //Approach:
-//DP solution
-//utilizes the solutions of previously calculated sums to  calculate the current sum
-//DP 2D Array: go through each individual sums + working out the total number of ways
+//using DP 2D array
 var combinationSum = (candidates, target) => {
-    //making 'candidates' as sorted - ascending order
+    //ascending order
     candidates.sort((a, b) => a - b);
-  
-    let dp = [[[]]]; //an array of arrays of arrays
-    
-    //loop through sum
+
+    //dp - array of arrays of arrays
+    let dp = [ [ [ ] ] ]; 
+
+    //looping
     for (let sum = 0; sum <= target; sum++) {
-      dp[sum] = [];
-  
-      //combine all the potential solutions for a given target
-      let combine = [];
-  
-      //loop through candidates
-      for (let i = 0; i < candidates.length && candidates[i] <= sum; i++) {
-        if (sum === candidates[i]) {
-          combine.push([candidates[i]]);
-        } else {
-          //need to check the previous values within DP and loop through those
-          for (let prev of dp[sum - candidates[i]]) {
-            //stop duplicates
-            if (candidates[i] >= prev[prev.length - 1]) { //if it is greater than the last value within prev
-              combine.push([...prev, candidates[i]]);
+        dp[sum] = [];
+
+        //to combine all potential solutions
+        let combine = [];
+
+        //looping through candidates
+        for (let i = 0; i < candidates.length && candidates[i] <= sum; i++) {
+            if (sum === candidates[i]) combine.push([candidates[i]]);
+            else {
+                //check prev val within DP
+                for (let prev of dp[sum - candidates[i]]) {
+                    if (candidates[i] >= prev[prev.length - 1]) combine.push([...prev, candidates[i]]);
+                }
             }
-          }
+            dp[sum] = combine;
         }
-        dp[sum] = combine;
-      }
     }
+
     return dp[target];
-  }
-  combinationSum([2,3,6,7], 7); //[[2,2,3], [7]] 
-  //DP - 2D array
-  //     0   1    2    3    4    5     6      7
-  // 2   []  []  [2]   [] [2,2] []  [2,2,2] []
-  // 3   []  []  []   [3]  [] [2,3] [3,3] [2,2,3]
-  // 6   []  []  []    []  []   []   [6]    []
-  // 7   []  []  []    []  []   []   []    [7]
+}
+combinationSum([2,3,6,7], 7); //[[2,2,3], [7]] 
+//DP 
+//     0   1    2    3   4    5     6     7                    0   1    2    3     4      5      6        7
+// 2   []  []  []   []   []   []   []    []               2    []  []  [2]   []  [2,2]   []    [2,2,2]   []
+// 3   []  []  []   []   []   []   []    []      ->       3    []  []  []    [3]   []   [2,3]   [3,3]   [2,2,3]
+// 6   []  []  []   []   []   []   []    []               6    []  []  []    []    []    []     [6]      []
+// 7   []  []  []   []   []   []   []    []               7    []  []  []    []    []    []     []       [7]
   
-  //0 []
-  //1 []
-  //2 [2]
-  //3 [3]
-  //4 [2,2]
-  //5 [2,3]
-  //6 [2,2,2], [3,3], [6]
-  //7 [2,2,3], [7] -> return
+//sorting: [2, 3, 6, 7] -> [2, 3, 6, 7]
+//sum = 0 - dp[0] = [], combine = [ ]
+//[2, 3, 6, 7]
+// ^
+//i = 0 -> 2 > 0   || i = 1 -> 3 > 0   || i = 2 -> 6 > 0   || i = 3 -> 7 > 0
+
+//sum = 1 -> dp[1] = [], combine = [ ]
+//[2, 3, 6, 7]
+// ^
+//i = 0 -> 2 > 1  || i = 1 -> 3 > 1   || i = 2 -> 6 > 1   || i = 3 -> 7 > 1
+
+//sum = 2 -> dp[2] = [], combine = [ ]
+//[2, 3, 6, 7]
+// ^
+//i = 0 -> 2 = 2  => combine = [ 2 ]
+//i = 1 -> 3 > 2  || i = 2 -> 6 > 2   || i = 3 -> 7 > 2
+//dp[2] = [2]
+
+//sum = 3 -> dp[3] = [], combine = [ ]
+//[2, 3, 6, 7]
+// ^  ^
+//i = 0 -> 2 <= 3                 
+//prev of dp[3 - 2] = dp[1]: []
+//i = 1 -> 3 <= 3  => combine = [ 3 ]
+//i = 2 -> 6 > 3   || i = 3 -> 7 > 3
+//dp[3] = [3]
+
+//sum = 4 -> dp[4] = [], combine = [ ]
+//[2, 3, 6, 7]
+// ^  ^
+//i = 0 -> 2 <= 4 => prev of dp[4 - 2] = dp[2]: [2]
+//2 >= prev[1 - 1] = 2 -> combine = [2, 2]
+//i = 1 -> 3 <= 4 => prev of dp[4 - 3] = dp[1]: []
+//i = 2 -> 6 > 4   || i = 3 -> 7 > 4
+//dp[4] = [2, 2]
+
+//sum = 5 -> dp[5] = [], combine = [ ]
+//[2, 3, 6, 7]
+// ^  ^
+//i = 0 -> 2 <= 5 => prev of dp[5 - 2] = dp[3]: [3]
+//2 < prev[1 - 1] = 3
+//i = 1 -> 3 <= 5 => prev of dp[5 - 3] = dp[2]: [2]
+//3 > prev[1 - 1] = 2 -> combine = [2, 3]
+//i = 2 -> 6 > 4   || i = 3 -> 7 > 4
+//dp[5] = [2, 3]
+
+//sum = 6 -> dp[6] = [], combine = [ ]
+//[2, 3, 6, 7]
+// ^  ^  ^
+//i = 0 -> 2 <= 6 => prev of dp[6 - 2] = dp[4]: [2, 2]
+//2 >= prev[2 - 1] = 2 -> combine = [2, 2, 2]
+//i = 1 -> 3 <= 6 => prev of dp[6 - 3] = dp[3]: [3]
+//3 = prev[1 - 1] = 3 -> combine = [3, 3]
+//i = 2 -> 6 = 6  => combine = [ 6 ]
+//dp[6] = [2, 2, 2], [3, 3], [6]
+
+//sum = 7 -> dp[7] = [], combine = [ ]
+//[2, 3, 6, 7]
+// ^  ^  ^  ^
+//i = 0 -> 2 <= 7 => prev of dp[7 - 2] = dp[5]: [2, 3]
+//2 < prev[2 - 1] = 3
+//i = 1 -> 3 <= 7 => prev of dp[7 - 3] = dp[4]: [2, 2]
+//3 > prev[2 - 1] = 2 -> combine = [2, 2, 3]
+//i = 2 -> 6 < 7  => prev of dp[7 - 6] = dp[]: [ ]
+//i = 3 -> 7 = 7  => combine = [ 7 ]
+//dp[6] = [2, 3], [2, 2, 3], [7]
+
+//0 []
+//1 []
+//2 [2]
+//3 [3]
+//4 [2,2]
+//5 [2,3]
+//6 [2,2,2], [3,3], [6]
+//7 [2,2,3], [7] -> return
   
-  combinationSum([2,3,5], 8); //[[2,2,2,2], [2,3,3], [3,5]]
-  //DP - 2D array
-  //     0   1    2    3    4    5     6      7        8
-  // 2   []  []  [2]   [] [2,2]  []  [2,2,2]  []   [2,2,2,2]
-  // 3   []  []  []    [3]  [] [2,3] [3,3]  [2,2,3]  [2,3,3]
-  // 5   []  []  []    []  []   [5]   []    [2,5]    [3,5]
+combinationSum([2,3,5], 8); //[[2,2,2,2], [2,3,3], [3,5]]
+//DP - 2D array
+//     0   1    2    3    4    5     6      7        8
+// 2   []  []  [2]   [] [2,2]  []  [2,2,2]  []   [2,2,2,2]
+// 3   []  []  []    [3]  [] [2,3] [3,3]  [2,2,3]  [2,3,3]
+// 5   []  []  []    []  []   [5]   []    [2,5]    [3,5]
   
-  //0 []
-  //1 []
-  //2 [2]
-  //3 [3]
-  //4 [2,2]
-  //5 [2,3], [5]
-  //6 [2,2,2], [3,3]
-  //7 [2,2,3], [2,5]
-  //8  [2,2,2,2], [2,3,3], [3,5] -> return
+//0 []
+//1 []
+//2 [2]
+//3 [3]
+//4 [2,2]
+//5 [2,3], [5]
+//6 [2,2,2], [3,3]
+//7 [2,2,3], [2,5]
+//8  [2,2,2,2], [2,3,3], [3,5] -> return
