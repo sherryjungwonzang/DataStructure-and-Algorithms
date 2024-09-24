db999c8838b8d22b5f045814a9cc4bc130c1102b
sherryjungwonzang <80478248+sherryjungwonzang@users.noreply.github.com>
2024-09-23 23:02:26 -0700

    Create 504-Base7.js

---
+++ LeetCode/String/504-Base7.js
@@ -0,0 +1,9 @@
+//504. Base7
+//given an integer num
+//return a string of its base 7 representation
+var base7 = (num) => {
+    return num.toString(7);
+}
+base7(100); //"202"
+
+base7(-7); //"-10"
