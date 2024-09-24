64491b4a03e2abd80faf31d5c40e27d057868142
sherryjungwonzang <80478248+sherryjungwonzang@users.noreply.github.com>
2024-09-23 23:27:52 -0700

    Create 507-PerfectNumber.js

---
+++ LeetCode/Array&Hash/507-PerfectNumber.js
@@ -0,0 +1,34 @@
+//507. Perfect Number
+//a perfect number is a positive integer that is equal to the sum of its positive divisors
+//excluding the number itself
+//a divisor of an integer x is an integer that can divide x evenly
+//given an integer n
+//return true if n is a perfect number, otherwise return false
+var perfectNumber = (num) => {
+    let count = 0;
+
+    for (let i = 1; i <= num / 2; i++) {
+        //perfect number
+        if (num % i === 0) count += 1;
+    }
+
+    return num === count;
+}
+perfectNumber(28); //true - 1 + 2 + 4 + 7 + 14 = 28
+//starting from 1 to 28 / 2 = 14
+//28 % 1 === 0 -> perfect number
+//count = 0 -> 1
+
+//28 % 2 === 0 -> perfect number
+//count = 0 -> 1 -> 3
+
+//28 % 4 === 0 -> perfect number
+//count = 0 -> 1 -> 3 -> 7
+
+//28 % 7 === 0 -> perfect number
+//count = 0 -> 1 -> 3 -> 7 -> 14
+
+//28 % 14 === 0 -> perfect number
+//count = 0 -> 1 -> 3 -> 7 -> 14 -> 28
+
+perfectNumber(7); //false
