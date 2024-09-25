f5bec2ae7e1a42981c711a6c51d54796e7b9568d
sherryjungwonzang <80478248+sherryjungwonzang@users.noreply.github.com>
2024-09-24 23:09:24 -0700

    Create 868-BinaryGap.js

---
+++ LeetCode/SlidingWindow/868-BinaryGap.js
@@ -0,0 +1,141 @@
+//868. Binary Gap
+//given a positive integer n
+//find and return the longest distance between any two adjacent 1's in the binary representation of n
+//if there are no two adjacent 1's, return 0
+
+//two 1's adjacent if there are only 0's separating them - possibly no 0's
+//the distance between two 1's is the absolute difference between their bit positions
+//for example, the two 1's in "1001" have a distance of 3
+
+//Approach:
+//using toString and sliding window
+var binaryGap = (n) => {
+    let binary = n.toString(2);
+    let res = 0;
+
+    //to calculate the length
+    let start = 0;
+    let end = 1;
+
+    //sliding window
+    for (let i = 0; i < binary.length; i++) {
+        if (binary[i] === "1") {
+            res = Math.max(res, i - start); //updating max length
+
+            start = i;
+
+            //reseting the length
+            end = 0;
+        } else { //"0"
+            end += 1;
+        }
+    }
+
+    return res;
+}
+//TC: O(n)
+//SC: O(1)
+binaryGap(22); //2
+//22 -> toString: 10110
+
+// 1 0 1 1 0
+// i
+// -
+//i = 1 -> max(0, 0 - 1) = 0
+//res = 0 -> 0
+//start = 0 -> 0
+//end = 1 -> 0 
+
+// 1 0 1 1 0
+//   i
+// ---
+//i = 0
+//res = 0 -> 0
+//start = 0 -> 0
+//end = 1 -> 0  -> 1
+
+// 1 0 1 1 0
+//     i
+// -----
+//i = 1 -> max(0, 2 - 0) = 2
+//res = 0 -> 0 -> 2
+//start = 0 -> 0 -> 2
+//end = 1 -> 0  -> 1 -> 0
+
+// 1 0 1 1 0
+//       i
+// -----
+//i = 1 -> max(2, 3 - 2) = 1
+//res = 0 -> 0 -> 2 -> 2
+//start = 0 -> 0 -> 2 -> 3
+//end = 1 -> 0  -> 1 -> 0 -> 0
+
+// 1 0 1 1 0
+//         i
+// -----
+//i = 0
+//res = 0 -> 0 -> 2 -> 2
+//start = 0 -> 0 -> 2 -> 3
+//end = 1 -> 0 -> 1 -> 0 -> 0 -> 1
+
+binaryGap(8); //0
+//8 -> toString: 1000
+
+// 1 0 0 0
+// i
+// -
+//i = 1 -> max(0, 0 - 1) = 0
+//res = 0 -> 0
+//start = 0 -> 0
+//end = 1 -> 0 
+
+// 1 0 0 0
+//   i
+// -
+//i = 0
+//res = 0 -> 0
+//start = 0 -> 0
+//end = 1 -> 0 -> 1
+
+// 1 0 0 0
+//     i 
+// -
+//i = 0
+//res = 0 -> 0
+//start = 0 -> 0
+//end = 1 -> 0 -> 1 -> 2
+
+// 1 0 0 0
+//       i 
+// -
+//i = 0
+//res = 0 -> 0
+//start = 0 -> 0
+//end = 1 -> 0 -> 1 -> 2 -> 3
+
+binaryGap(5); //2
+//5 -> toString: 101
+
+// 1 0 1
+// i
+// -
+//i = 1 -> max(0, 0 - 1) = 0
+//res = 0 -> 0
+//start = 0 -> 0
+//end = 1 -> 0 
+
+// 1 0 1
+//   i
+// ---
+//i = 0
+//res = 0 -> 0
+//start = 0 -> 0
+//end = 1 -> 0 -> 1
+
+// 1 0 1
+//     i
+// ---
+//i = 1 -> (0, 2 - 0) = 2
+//res = 0 -> 0 -> 2
+//start = 0 -> 0 -> 2
+//end = 1 -> 0 -> 1 -> 0
