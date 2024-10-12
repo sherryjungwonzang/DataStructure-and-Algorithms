a3d2e8cf2004569664e24ab9de552b6f9857d082
sherryjungwonzang <80478248+sherryjungwonzang@users.noreply.github.com>
2024-10-12 02:19:57 -0700

    Update 216-CombinationSum3.js

--- LeetCode/BackTracking/216-CombinationSum3.js
+++ LeetCode/BackTracking/216-CombinationSum3.js
@@ -6,34 +6,37 @@
 //the list must not contain the same combination twice, and the combinations maybe returned in any order
 
 //Approach:
-//using DFS
-var combinationSum3 = (k, n) => {
-  let res = [];
+//using DFS with backtracking
+var combinationSum2 = (candidates, target) => {
+    let res = [];
 
-  //DFS
-  function dfs(index, curr, total) {
-    //when we cannot find it
-    if (total < 0 || curr.length > k) return;
+    //DFS
+    function dfs(index, curr, total) {
+        //we cannot find
+        if (total < 0 || curr.length > k) return;
 
-    //when we find it
-    if (total === 0 && curr.length === k) res.push([...curr]);
+        //we can find
+        if (total === 0 && curr.length === k) res.push([...curr]);
 
-    //looping through the values
-    for (let i = index; i <= 9; i++) {
-      curr.push(i);
+        //looping
+        for (let i = index; i <= 9; i++) {
+            curr.push(i);
 
-      //recursive call
-      dfs(i + 1, curr, total - i);
+            //recursiva call
+            dfs(i + 1, curr, total - i);
 
-      curr.pop();
+            //backtracking
+            curr.pop();
+        }
     }
-  }
-  dfs(1, [], n); //since it is starting from 1
 
-  return res;
+    dfs(1, [], n);
+
+    return res;
 }
 combinationSum3(3, 7); //[[1,2,4]] - 1+2+4=7 : there are no other valid combinations
 
 combinationSum3(3, 9); //[[1,2,6], [1,3,5], [2,3,4]]; //1+2+6=9, 1+3+5=9, 2+3+4=9 : there are no other valid combinations
 
 combinationSum3(4, 1); //[] - there are no valid combinations : using 4 different numbers in the range [1,9]- the smallest sum we can get is 1+2+3+4=10 and since 10 > 1, there are no valid combination
+
