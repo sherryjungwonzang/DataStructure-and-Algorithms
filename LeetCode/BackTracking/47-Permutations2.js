298a297e3f054b00460b3c7c0b9cf2df0a50544c
sherryjungwonzang <80478248+sherryjungwonzang@users.noreply.github.com>
2024-10-12 03:09:34 -0700

    Update 47-Permutations2.js

--- LeetCode/BackTracking/47-Permutations2.js
+++ LeetCode/BackTracking/47-Permutations2.js
@@ -3,40 +3,45 @@
 //return all possible unique permutations in any order
 
 //Approach:
-//using backtracking and DFS
+//using DFS with backtracking
 var permutations2 = (nums) => {
-  let res = [];
-  let visited = new Array(nums.length).fill(0);
+    let res = [];
+    let visited = new Array(nums.length).fill(0);
 
-  //sorting
-  nums.sort((a, b) => a - b);
+    //sorting
+    nums.sort((a, b) => a - b);
 
-  //DFS
-  function dfs(index, nums, arr, res) {
-    if (index === nums.length) { //meaning looping all numbers
-      res.push([...arr]);
-      return;
+    //DFS
+    function dfs(idx, nums, arr, res) {
+        //base case
+        if (idx === nums.length) {
+            res.push([...arr]);
+
+            return;
+        }
+
+        //recursive calls
+        for (let i = 0; i < nums.length; i++) {
+            if (visited[i] === 0) {
+                arr.push(nums[i]);
+                visited[i] = 1;
+
+                //recursive call
+                dfs(idx + 1, nums, arr, res);
+
+                //backtracking
+                arr.pop();
+
+                visited[i] = 0;
+
+                //avoid duplicates
+                while (nums[i] === nums[i + 1]) i++;
+            }
+        }
     }
 
-    for (let i = 0; i < nums.length; i++) {
-      if (visited[i] === 0) {
-        arr.push(nums[i]);
-        visited[i] = 1;
+    dfs(0, nums, [], res);
 
-        dfs(index + 1, nums, arr, res);
-
-        arr.pop(); //backtracking
-        visited[i] = 0; //resetting as 0
-
-        while (nums[i] === nums[i + 1]) i++; //avoiding the duplicates
-      }
-    }
-  }
-
-  dfs(0, nums, [], res);
-
-  return res;
+    return res;
 }
 permutations2([1,1,2]); //[[1,1,2], [1,2,1], [2,1,1]]
-
-permutations2([1,2,3]); //[[1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], [3,2,1]]
