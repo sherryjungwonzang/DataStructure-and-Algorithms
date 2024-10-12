5aed796fcc0b421e1eb7d0e4bedc080677eff1c8
sherryjungwonzang <80478248+sherryjungwonzang@users.noreply.github.com>
2024-10-12 02:24:38 -0700

    Update 52-N-Queens2.js

--- LeetCode/BackTracking/52-N-Queens2.js
+++ LeetCode/BackTracking/52-N-Queens2.js
@@ -2,53 +2,52 @@
 //the n-queens puzzle is the problem of placing 'n' queens on an n x n chessboard such that no two queens attack each other
 //given an integer 'n'
 //return the number of distinct solutions to the n-queens puzzle
+
+//Approach:
+//using recursion and backtracking
 var nQueens2 = (n) => {
-  //variables
-  let col = new Set();
-  let posDiag = new Set();
-  let negDiag = new Set();
-  let count = 0;
+    let col = new Set(); 
+    let posDiag = new Set();
+    let negDiag = new Set();
+    let count = 0;
 
-  //helper function
-  //1. isValid - to check whether the column has an attacking queen or not
-  //whether posDiag or negDiag
-  const isValid = (r, c) => !(col.has(c) || posDiag.has(r + c) || negDiag.has(r - c));
+    //checking validation - posDiag: r + c, negDiag: r - c
+    const isValid = (r, c) => !(col.has(c) || posDiag.has(r + c) || negDiag.has(r - c));
 
-  //2. addQueen - for adding the queen to its position
-  //within the positive diagonal and the negative diagonal
-  const addQueen = (r, c) => {
-    col.add(c);
-    posDiag.add(r + c);
-    negDiag.add(r - c);
-  }
+    //adding queen
+    function addQueen(r, c) {
+        col.add(c);
+        posDiag.add(r + c);
+        negDiag.add(r - c);
+    };
 
-  //3. removeQueen - to remove queens when doing backtracking
-  const removeQueen = (r, c) => {
-    col.delete(c);
-    posDiag.delete(r + c);
-    negDiag.delete(r - c);
-  }
+    //removing queen
+    function removeQueen(r, c) {
+        col.delete(c);
+        posDiag.delete(r + c);
+        negDiag.delete(r - c);
+    };
 
-  //backtracking
-  function recurse(row) {
-    //base case
-    if (row === n) count++;
+    //recursive backtracking
+    function recurse(row) {
+        //base case
+        if (row === n) count++;
 
-    //looping through columns
-    for (let col = 0; col < n; col++) {
-      if (isValid(row, col)) {
-        addQueen(row, col);
+        //recursive calls
+        for (let col = 0; col < n; col++) {
+            if (isValid(row, col)) {
+                addQueen(row, col);
 
-        //update count after recursing the next row
-        count = recurse(row + 1, count);
+                count = recurse(row + 1, count);
 
-        //backtracking
-        removeQueen(row, col);
-      }
-    }
-    return count;
-  }
-  return recurse(0);
+                removeQueen(row, col);
+            }
+        }
+
+        return count;
+    };
+
+    return recurse(0);
 }
 nQueens2(4); //2 - there are two distinct solutions to the 4-queens puzzle as shown
 
