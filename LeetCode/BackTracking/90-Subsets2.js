9de41310fbf7e814ad5246c791e39bb5163f0b51
sherryjungwonzang <80478248+sherryjungwonzang@users.noreply.github.com>
2024-10-12 02:14:34 -0700

    Update 90-Subsets2.js

--- LeetCode/BackTracking/90-Subsets2.js
+++ LeetCode/BackTracking/90-Subsets2.js
@@ -3,31 +3,36 @@
 //return all possible subsets
 //the solution set must not contain duplicate subsets
 //return the solution in any order
+
+//Approach:
+//using DFS with backtracking
 var subsets2 = (nums) => {
-  let res = [ [] ];
+    let res = [ [] ];
 
-  //sorting the nums array for avoid checking the same value
-  nums.sort((a, b) => a - b);
+    //sorting
+    nums.sort((a, b) => a - b);
 
-  //DFS
-  function dfs(nums, res, curr, index) {
-    for (let i = index; i < nums.length; i++) {
-      //check whether they have any duplications or not
-      if (i === index || nums[i] !== nums[i - 1]) {
-        curr.push(nums[i]);
-        res.push([...curr]);
+    //DFS
+    function dfs(nums, res, curr, index) {
+        for (let i = index; i < nums.length; i++) {
+            //checking whether they have any duplicates or not
+            if (i === index || nums[i] !== nums[i - 1]) {
+                curr.push(nums[i]);
 
-        //recursive call
-        dfs(nums, res, curr, i + 1);
-        
-        //backtracking
-        curr.pop();
-      }
+                res.push([...curr]);
+
+                //recursive call
+                dfs(nums, res, curr, i + 1);
+
+                //backtracking
+                curr.pop();
+            }
+        }
     }
-  }
-  dfs(nums, res, [], 0);
 
-  return res;
+    dfs(nums, res, [], 0);
+
+    return res;
 }
 subsets2([1, 2, 2]); //[[],[1],[1,2],[1,2,2],[2],[2,2]]
 
