0ef3aefa2aa32680783f077d7c942f9a5abad341
sherryjungwonzang <80478248+sherryjungwonzang@users.noreply.github.com>
2024-10-12 02:15:41 -0700

    Update 78-Subsets.js

--- LeetCode/BackTracking/78-Subsets.js
+++ LeetCode/BackTracking/78-Subsets.js
@@ -3,28 +3,32 @@
 //return all possible subsets
 //the solution set must not contain duplicate subsets
 //return the solution in any order
+
+//Approach:
+//using DFS with backtracking
 var subsets = (nums) => {
     //setting res array with an empty array as default value
     let res = [ [] ];
-  
-    //DFS function
+
+    //DFS
     function dfs(index, curr) {
-      for (let i = index; i < nums.length; i++) {
-        curr.push(nums[i]);
-        res.push([...curr]); //pushing into res array right after we collects in curr array
-  
-        //recursion
-        dfs(i + 1, curr);
-  
-        //backtracking
-        curr.pop();
-      }
+        for (let i = index; i < nums.length; i++) {
+            curr.push(nums[i]);
+
+            res.push([...curr]); 
+
+            //recursive call
+            dfs(i + 1, curr);
+
+            //backtracking
+            curr.pop();
+        }
     }
+
     dfs(0, []);
-  
+
     return res;
 }
-  
 subsets([1, 2, 3]); //[[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
   
 subsets([0]); //[[],[0]]
