e0390ec473d04f4dbc5f4f2ae53f60bafd88078d
sherryjungwonzang <80478248+sherryjungwonzang@users.noreply.github.com>
2024-11-05 15:41:51 -0800

    Update 3095-ShortestSubarrayWithORAtLeastK.js

--- LeetCode/BitManipulation/3095-ShortestSubarrayWithORAtLeastK.js
+++ LeetCode/BitManipulation/3095-ShortestSubarrayWithORAtLeastK.js
@@ -29,7 +29,7 @@ shortestSubarrayOR(nums = [1,2,3], k = 2); //1
 // i                                                 i                                                   i
 // j                                                    j                                                      j
 //curr = 1 -> 1 | 1 = 1                             curr = 1 -> 1 | 2 = 3                               curr = 3 -> 3 | 3 = 3
-//curr < k: F && Infinity > 0 - 0 + 1: T = F        curr > k: T && Infinity > 1 - 0 + 1: T = T          curr > k: T && 2 < 2 - 0 + 1: F = F
+//curr = k: F && Infinity > 0 - 0 + 1: T = F        curr > k: T && Infinity > 1 - 0 + 1: T = T          curr > k: T && 2 < 2 - 0 + 1: F = F
 //min = Infi -> Infi                                min = Infi -> Infi -> 2                             min = Infi -> Infi -> 2 -> 2                
 
 //[1, 2, 3]                                         [1, 2, 3]                                           
@@ -57,8 +57,15 @@ shortestSubarrayOR(nums = [2,1,8], k = 10); //3
 //[2, 1, 8]                                         [2, 1, 8] 
 //    i                                                 i   
 //    j                                                    j      
-//curr = 1 -> 11 | 1 = 1                            curr = 1 -> 1 | 8 = 9     
+//curr = 1 -> 1 | 1 = 1                            curr = 1 -> 1 | 8 = 9     
 //curr < k: F && 3 > 0 - 0 + 1: T = F               curr < k: F && 3 > 2 - 1 + 1: T = F     
 //min = Infi -> Infi -> Infi -> 3 -> 3              min = Infi -> Infi -> Infi -> 3 -> 3 -> 3
+
+//[2, 1, 8]   
+//       i   
+//       j        
+//curr = 8 -> 9 | 8 = 8       
+//curr < k: F && 3 > 2 - 2 + 1: T = F     
+//min = Infi -> Infi -> Infi -> 3 -> 3 -> 3   
 
 shortestSubarrayOR(nums = [1,2], k = 0); //1
